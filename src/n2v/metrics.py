c4d9df99a23e6fd0cf3488f5d3cfefbd5b00883a
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-04-14 15:47:34 +0200

    Add metrics test

--- src/n2v/metrics.py
+++ src/n2v/metrics.py
@@ -2,15 +2,46 @@ import numpy as np
 from skimage.metrics import peak_signal_noise_ratio
 
 
-def psnr(gt, pred, range=255.0):
+def psnr(gt: np.ndarray, pred: np.ndarray, range: float =255.0) -> float:
+    """Peak Signal to Noise Ratio
+
+    This method calls skimage.metrics.peak_signal_noise_ratio. See:
+    https://scikit-image.org/docs/dev/api/skimage.metrics.html
+
+    Parameters
+    ----------
+    gt : NumPy array
+        Ground truth image
+    pred : NumPy array
+        Predicted image
+    range : float, optional
+        The images pixel range, by default 255.0
+
+    Returns
+    -------
+    float
+        PSNR value
+    """    
     return peak_signal_noise_ratio(gt, pred, data_range=range)
 
 
-def zero_mean(x):
+def zero_mean(x: np.ndarray) -> np.ndarray:
+    """Zero the mean of an array.
+
+    Parameters
+    ----------
+    x : NumPy array
+        Input array
+
+    Returns
+    -------
+    NumPy array
+        Zero-mean array
+    """    
     return x-np.mean(x)
 
 
-def fix_range(gt,x):
+def fix_range(gt: np.ndarray, x):
     a = np.sum(gt*x) / (np.sum(x*x))
     return x*a
 
@@ -20,20 +51,20 @@ def fix(gt,x):
     return fix_range(gt_, zero_mean(x))
 
 
-def scale_psnr(gt, pred):
-    """Scale invariant PSNR
+def scale_invariant_psnr(gt: np.ndarray, pred: np.ndarray) -> float:
+    """Scale invariant PSNR.
 
     Parameters
     ----------
-    gt : _type_
-        _description_
-    pred : _type_
-        _description_
+    gt : NumPy array
+        Ground truth image
+    pred : NumPy array
+        Predicted image
 
     Returns
     -------
     Callable
-        _description_
+        Scale invariant PSNR
     """
     range = (np.max(gt) - np.min(gt)) / np.std(gt)
     gt_ = zero_mean(gt) / np.std(gt)
@@ -41,17 +72,34 @@ def scale_psnr(gt, pred):
 
 
 class MetricTracker:
+    """Metric tracker
 
-    def __init__(self):
+    This class is used to track values, sum, count and average of a metric over time.
+
+    Attributes
+    ----------
+    val : int
+        Last value of the metric
+    avg : float
+        Average value of the metric
+    sum : int
+        Sum of the metric values (times number of values)
+    count : int
+        Number of values
+    """    
+
+    def __init__(self) -> None:
         self.reset()
 
-    def reset(self):
+    def reset(self) -> None:
+        """Reset the metric tracker state.
+        """        
         self.val = 0
         self.avg = 0
         self.sum = 0
         self.count = 0
 
-    def update(self, value: int, n: int = 1):
+    def update(self, value: int, n: int = 1) -> None:
         """Update the metric tracker state
 
         Parameters
---
+++ tests/test_metrics.py
@@ -0,0 +1,49 @@
+import pytest
+
+import numpy as np
+
+from n2v.metrics import (
+    zero_mean,
+    scale_invariant_psnr,
+    MetricTracker,
+)
+
+# TODO tests fix and fix_range
+
+@pytest.mark.parametrize("x", [
+        5.6,
+        np.array([1, 2, 3, 4, 5]),
+        np.array([[1, 2, 3], [4, 5, 6]]),
+    ]
+)
+def test_zero_mean(x):
+    assert np.allclose(zero_mean(x), x-np.mean(x))
+
+
+@pytest.mark.parametrize("gt, pred, result", [
+        (np.array([1, 2, 3, 4, 5, 6]), np.array([1, 2, 3, 4, 5, 6]), 332.22),
+        (np.array([[1, 2, 3], [4, 5, 6]]), np.array([[1, 2, 3], [4, 5, 6]]), 332.22),
+])
+def test_scale_invariant_psnr(gt, pred, result):
+    assert scale_invariant_psnr(gt, pred) == pytest.approx(result, rel = 5e-3)
+
+
+def test_metric_tracker():
+    tracker = MetricTracker()
+
+    # check initial state
+    assert tracker.sum == 0
+    assert tracker.count == 0
+    assert tracker.avg == 0
+    assert tracker.val == 0
+
+    # run a few updates
+    n = 5
+    for i in range(n):
+        tracker.update(i, n)
+
+    # check values
+    assert tracker.sum == n * ( n * (n-1)) / 2
+    assert tracker.count == n*n
+    assert tracker.avg == (n-1) / 2
+    assert tracker.val == n-1
