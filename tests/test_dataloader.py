0daf0e6f8a01eb5456bb61bd5a6aafe0d9ab151e
CatEek <zubarev.ia@gmail.com>
2023-04-20 17:33:05 +0200

    change test dataloader

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,16 +1,14 @@
-import pytest 
+import pytest
 import tifffile
 
 import numpy as np
 
 from n2v.dataloader import (
     list_input_source_tiff,
-    _calculate_stitching_coords,
-    extract_patches_sequential
+    extract_patches_sequential,
+    PatchDataset,
 )
-from n2v.dataloader_utils.dataloader_utils import (
-    _compute_number_of_patches
-)
+from n2v.dataloader_utils.dataloader_utils import _compute_number_of_patches
 
 
 def test_list_input_source_tiff(tmp_path):
@@ -20,10 +18,7 @@ def test_list_input_source_tiff(tmp_path):
     # create np arrays
     arrays = []
     for n in range(num_files):
-        arr_list = [
-            [i for i in range(2+n)],
-            [i*i for i in range(2+n)]
-        ]
+        arr_list = [[i for i in range(2 + n)], [i * i for i in range(2 + n)]]
         arrays.append(np.array(arr_list))
 
     # create tiff files
@@ -41,66 +36,86 @@ def test_list_input_source_tiff(tmp_path):
     assert len(set(list_of_files)) == 2
 
 
-@pytest.mark.parametrize("tile_coords, last_tile_coords, overlap, expected", 
-                         [
-                                # (y, x) [...] (start_y, end_y, start_x, end_x)      
+@pytest.mark.parametrize(
+    "arr_shape, axes, patch_size",
+    [
+        ((8, 8), "YX", (4, 4)),
+    ],
+)
+def test_patch_dataset_read_source_errors(tmp_path, arr_shape, axes, patch_size):
+    arr = np.arange(np.prod(arr_shape)).reshape(arr_shape)
 
-                                # 2D: top left corner
-                                ((0, 0), (3, 5), (10, 8), (0, -5, 0, -4)),
+    path = tmp_path / "test.tif"
+    tifffile.imwrite(path, arr)
+    assert path.exists()
 
-                                # 2D: top right corner
-                                ((0, 4), (3, 5), (10, 8), (0, -5, 4, None)),
-
-                                # 2D: bottom left corner
-                                ((2, 0), (3, 5), (10, 8), (5, None, 0, -4)),
-
-                                # 2D: bottom right corner
-                                ((2, 4), (3, 5), (10, 8), (5, None, 4, None)),
-
-                                # 2D middle
-                                ((1, 1), (3, 5), (10, 8), (5, -5, 4, -4)),
-
-                                # 3D: front (bottom left)
-                                ((0, 2, 0), (7, 3, 5), (6, 10, 8), (0, -3, 5, None, 0, -4)),
-
-                                # 3D: back (bottom left)
-                                ((6, 2, 0), (7, 3, 5), (6, 10, 8), (3, None, 5, None, 0, -4)),
-                         ])
-def test_calculate_stitching_coords(tile_coords, last_tile_coords, overlap, expected):
-    """Test calculating stitching coordinates"""
-    expected_slices = [
-        slice(expected[2*i], expected[2*i+1]) for i in range(len(overlap))
-    ]
-
-    # compute stitching coordinates
-    result = _calculate_stitching_coords(tile_coords, last_tile_coords, overlap)
-    assert result == expected_slices
+    with pytest.raises(ValueError):
+        PatchDataset.read_data_source(path, patch_size)
 
 
-@pytest.mark.parametrize("arr_shape, patch_size", 
-                         [
-                            # Wrong number of dimensions 2D
-                            ((10, 10), (5, )),
-                            ((10, 10), (5, 5)), # minimum 3 dimensions CYX
-                            ((10, 10), (5, 5, 5)),
-                            ((1, 10, 10), (5, )),
-                            ((1, 1, 10, 10), (5, )),
+@pytest.mark.parametrize(
+    "arr_shape, axes, patch_size",
+    [
+        # All possible input variations (S(B), T(o), C(o), Z(o), Y, X)
+        # -> ((S(B) * T * P, C(o), Z(o), Y, X)
+        # 2D (S(B), C, Y, X)
+        # 3D (S(B), C, Z, Y, X)
+        # 2D
+        ((8, 8), "YX", (4, 4)),
+        ((1, 8, 8), "CYX", (4, 4)),
+        ((2, 8, 8), "CYX", (4, 4)),
+        # # 2D time series
+        ((10, 8, 8), "TYX", (4, 4)),
+        # (10, 1, 8, 8),
+        # (10, 2, 8, 8),
+        # # 3D
+        ((4, 8, 8), "ZYX", (4, 4, 4)),
+        ((8, 8, 8), "ZYX", (4, 4, 4)),
+        ((1, 4, 8, 8), "CZYX", (4, 4, 4)),
+        # # 3D time series
+        # (10, 32, 64, 64),
+    ],
+)
+def test_patch_dataset_read_source(tmp_path, arr_shape, axes, patch_size):
+    arr = np.arange(np.prod(arr_shape)).reshape(arr_shape)
 
-                            # Wrong number of dimensions 3D
-                            ((10, 10, 10), (5, 5, 5, 5)),
-                            ((1, 10, 10, 10), (5, 5)),
-                            ((1, 10, 10, 10), (5, 5, 5, 5)),
-                            ((1, 1, 10, 10, 10), (5, 5)),
-                            ((1, 1, 10, 10, 10), (5, 5, 5, 5)),
+    path = tmp_path / "test.tif"
+    tifffile.imwrite(path, arr)
+    assert path.exists()
 
-                            # Wrong z patch size
-                            ((1, 10, 10), (5, 5, 5)),
-                            ((10, 10, 10), (10, 5, 5)),
+    image, updated_patch_size, _ = PatchDataset.read_data_source(path, patch_size)
 
-                            # Wrong YX patch sizes
-                            ((1, 10, 10), (12, 5)),
-                            ((1, 10, 10), (5, 11)),
-                        ])
+    if axes == "YX":
+        assert image.shape == (1,) + arr_shape
+    elif axes == "CYX":
+        assert image.shape == arr_shape
+    elif axes == "TYX":
+        assert image.shape == (arr_shape[0], 1) + arr_shape[1:]
+
+
+@pytest.mark.parametrize(
+    "arr_shape, patch_size",
+    [
+        # Wrong number of dimensions 2D
+        ((10, 10), (5,)),
+        ((10, 10), (5, 5)),  # minimum 3 dimensions CYX
+        ((10, 10), (5, 5, 5)),
+        ((1, 10, 10), (5,)),
+        ((1, 1, 10, 10), (5,)),
+        # Wrong number of dimensions 3D
+        ((10, 10, 10), (5, 5, 5, 5)),
+        ((1, 10, 10, 10), (5, 5)),
+        ((1, 10, 10, 10), (5, 5, 5, 5)),
+        ((1, 1, 10, 10, 10), (5, 5)),
+        ((1, 1, 10, 10, 10), (5, 5, 5, 5)),
+        # Wrong z patch size
+        ((1, 10, 10), (5, 5, 5)),
+        ((10, 10, 10), (10, 5, 5)),
+        # Wrong YX patch sizes
+        ((1, 10, 10), (12, 5)),
+        ((1, 10, 10), (5, 11)),
+    ],
+)
 def test_extract_patches_sequential_invalid_arguments(arr_shape, patch_size):
     arr = np.zeros(arr_shape)
 
@@ -109,20 +124,10 @@ def test_extract_patches_sequential_invalid_arguments(arr_shape, patch_size):
 
         # get next yielded value
         next(patches_generator)
-        
 
-@pytest.mark.parametrize("overlaps", 
-                         [
-                            (3, 2),
-                            (2, 1),
-                            None
-                         ])
-@pytest.mark.parametrize("patch_size",
-                         [
-                            (5, 5),
-                            (6, 3),
-                            (6, 6)
-                         ])
+
+@pytest.mark.parametrize("overlaps", [(3, 2), (2, 1), None])
+@pytest.mark.parametrize("patch_size", [(5, 5), (6, 3), (6, 6)])
 def test_extract_patches_sequential_2d(array_2D, patch_size, overlaps):
     """Test extracting patches sequentially in 2D"""
     patch_generator = extract_patches_sequential(array_2D, patch_size, overlaps)
@@ -139,7 +144,7 @@ def test_extract_patches_sequential_2d(array_2D, patch_size, overlaps):
         n_patches = _compute_number_of_patches(array_2D, patch_size)
     else:
         n_patches = [
-            (array_2D.shape[i+1] - patch_size[i]) // (patch_size[i] - overlaps[i]) + 1
+            (array_2D.shape[i + 1] - patch_size[i]) // (patch_size[i] - overlaps[i]) + 1
             for i in range(len(patch_size))
         ]
 
@@ -147,24 +152,11 @@ def test_extract_patches_sequential_2d(array_2D, patch_size, overlaps):
 
 
 # TODO case (2, 3, 5), None doesn't work
-@pytest.mark.parametrize("patch_size",
-                            [
-                                (3, 5, 5),
-                                (5, 5, 5),
-                                (3, 3, 5),
-                                (4, 6, 6)
-                            ])
-@pytest.mark.parametrize("overlaps",
-                            [
-                                (0, 2, 1),
-                                (1, 1, 2),
-                                (1, 2, 1),
-                                (2, 1, 2),
-                                None
-                            ])
+@pytest.mark.parametrize("patch_size", [(3, 5, 5), (5, 5, 5), (3, 3, 5), (4, 6, 6)])
+@pytest.mark.parametrize("overlaps", [(0, 2, 1), (1, 1, 2), (1, 2, 1), (2, 1, 2), None])
 def test_extract_patches_sequential_3d(array_3D, patch_size, overlaps):
     """Test extracting patches sequentially in 3D"""
-    # compute expected number of patches   
+    # compute expected number of patches
     patch_generator = extract_patches_sequential(array_3D, patch_size, overlaps)
 
     # check individual patch shape
@@ -178,7 +170,7 @@ def test_extract_patches_sequential_3d(array_3D, patch_size, overlaps):
         n_patches = _compute_number_of_patches(array_3D, patch_size)
     else:
         n_patches = [
-            (array_3D.shape[i+1] - patch_size[i]) // (patch_size[i] - overlaps[i]) + 1
+            (array_3D.shape[i + 1] - patch_size[i]) // (patch_size[i] - overlaps[i]) + 1
             for i in range(len(patch_size))
         ]
 
