1ed225a92392f91a562d27915f1d01c4eb175437
veegalinova <veegalinova@gmail.com>
2023-07-05 15:44:19 +0200

    remove logger setting

--- src/careamics_restoration/utils/__init__.py
+++ src/careamics_restoration/utils/__init__.py
@@ -1,4 +1,3 @@
 from .validators import check_axes_validity
 from .normalization import denormalize, normalize
 from .torch_utils import get_device, setup_cudnn_reproducibility
-from .utils import set_logging
