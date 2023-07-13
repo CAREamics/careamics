996ae60c0091cabad79657ad59ec4e698f8fafc3
veegalinova <veegalinova@gmail.com>
2023-07-13 10:49:33 +0200

    fix type annotations in smoke test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,5 +1,6 @@
 import tempfile
 from pathlib import Path
+from typing import Tuple
 
 import numpy as np
 import pytest
@@ -17,14 +18,15 @@ from careamics_restoration.engine import Engine
 TEST_IMAGE_SIZE = (128, 128)
 TEST_PATCH_SIZE = (64, 64)
 
+
 @pytest.fixture
-def temp_dir():
+def temp_dir() -> Path:
     with tempfile.TemporaryDirectory() as temp_dir:
         yield Path(temp_dir)
 
 
 @pytest.fixture
-def example_data(temp_dir):
+def example_data_path(temp_dir: Path) -> Tuple[Path, Path]:
     test_image = np.random.rand(*TEST_IMAGE_SIZE)
 
     train_path = temp_dir / "train"
@@ -39,8 +41,8 @@ def example_data(temp_dir):
 
 
 @pytest.fixture
-def base_configuration(temp_dir, example_data):
-    train_dir, val_dir = example_data
+def base_configuration(temp_dir: Path, example_data_path: Tuple[Path, Path]) -> Configuration:
+    train_dir, val_dir = example_data_path
     configuration = Configuration(
         experiment_name="smoke_test",
         working_directory=temp_dir,
@@ -57,8 +59,8 @@ def base_configuration(temp_dir, example_data):
             patch_size=TEST_PATCH_SIZE,
             batch_size=1,
             optimizer=Optimizer(name="Adam"),
-            lr_scheduler=LrScheduler(name='ReduceLROnPlateau'),
-            extraction_strategy='random',
+            lr_scheduler=LrScheduler(name="ReduceLROnPlateau"),
+            extraction_strategy="random",
             augmentation=True,
             num_workers=0,
         ),
@@ -67,25 +69,16 @@ def base_configuration(temp_dir, example_data):
     return configuration
 
 
-@pytest.fixture
-def predict_configuration(base_configuration):
-    predict_configuration = base_configuration.model_copy()
-    workdir = predict_configuration.working_directory
-    model_name = f'{predict_configuration.experiment_name}_best.pth'
-    predict_configuration.trained_model = workdir / model_name
-    return predict_configuration
-
-
-def dump_config(configuration):
+def dump_config(configuration: Configuration) -> Path:
     temp_dir = configuration.working_directory
     config_path = temp_dir / "test_config.yml"
     config_dict = configuration.model_dump()
-    with open(config_path, 'w') as config_file:
+    with open(config_path, "w") as config_file:
         yaml.dump(config_dict, config_file)
     return config_path
 
 
-def test_is_engine_runnable(base_configuration):
+def test_is_engine_runnable(base_configuration: Configuration):
     """
     Test if basic workflow does not fail - train model and then predict
     """
@@ -95,7 +88,7 @@ def test_is_engine_runnable(base_configuration):
     engine = Engine(config_path)
     engine.train()
 
-    model_name = f'{engine.cfg.experiment_name}_best.pth'
+    model_name = f"{engine.cfg.experiment_name}_best.pth"
     result_model_path = engine.cfg.working_directory / model_name
 
     assert result_model_path.exists()
