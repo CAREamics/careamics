27f60c76f42d5686733f0b253a783147366ffd29
Mehdi Seifi <mehdiseifi@gmail.com>
2023-07-17 17:59:34 +0200

    fix export test (having a temp weights file)

--- tests/bioimage/test_export.py
+++ tests/bioimage/test_export.py
@@ -8,13 +8,19 @@ from careamics_restoration.engine import Engine
 
 def test_bioimage_export_default(minimum_config: dict, tmp_path: Path):
     """Test model export to bioimage forrmat by using default specs."""
-    # dump configs to a tmp file.
+    # dump configs to a tmp file:
     config_file = tmp_path.joinpath("tmp_config.yml")
-    with open(config_file, 'w') as f:
+    with open(config_file, "w") as f:
         yaml.safe_dump(minimum_config, f)
-    # create an engine to export
+    # create an engine to export the model:
     engine = Engine(config_file)
+    # save a tmp model's weights file
+    weight_file = tmp_path.joinpath(f"{minimum_config['experiment_name']}_best.pth")
+    with open(weight_file, "wb") as f:
+        f.write(b"model weights!")
+    # output zip file
     zip_file = tmp_path.joinpath("tmp_model.zip")  # model zip file
-    engine.save_as_bioimage(zip_file)
+    # export the model (overriding the weight_uri)
+    engine.save_as_bioimage(zip_file, {"weight_uri": str(weight_file)})
 
     assert zip_file.exists()
