acb99064b69d1f4e58294111a1315dcc62ffac89
Mehdi Seifi <mehdiseifi@gmail.com>
2023-07-18 14:20:28 +0200

    add import test

--- src/careamics_restoration/bioimage/__init__.py
+++ src/careamics_restoration/bioimage/__init__.py
@@ -2,12 +2,14 @@
 
 from .io import (
     build_zip_model, get_default_model_specs,
+    import_bioimage_model,
     PYTORCH_STATE_DICT
 )
 
 
 __all__ = [
     "build_zip_model",
+    "import_bioimage_model",
     "get_default_model_specs",
     "PYTORCH_STATE_DICT"
 ]
--- tests/bioimage/test_io.py
+++ tests/bioimage/test_io.py
@@ -4,9 +4,11 @@ import yaml
 import pytest
 
 from careamics_restoration.engine import Engine
+from careamics_restoration.config import Configuration
+from careamics_restoration.bioimage import import_bioimage_model
 
 
-def test_bioimage_export_default(minimum_config: dict, tmp_path: Path):
+def test_bioimage_export_default(minimum_config: dict, tmp_path: Path, request):
     """Test model export to bioimage forrmat by using default specs."""
     # dump configs to a tmp file:
     config_file = tmp_path.joinpath("tmp_config.yml")
@@ -19,8 +21,20 @@ def test_bioimage_export_default(minimum_config: dict, tmp_path: Path):
     with open(weight_file, "wb") as f:
         f.write(b"model weights!")
     # output zip file
-    zip_file = tmp_path.joinpath("tmp_model.zip")  # model zip file
+    zip_file = tmp_path.joinpath("tmp_model.zip")
     # export the model (overriding the weight_uri)
     engine.save_as_bioimage(zip_file, {"weight_uri": str(weight_file)})
 
+    # put zip file in the cache for the import test
+    request.config.cache.set("bioimage_model", str(zip_file))
+
     assert zip_file.exists()
+
+
+def test_bioimage_import(request):
+    zip_file = request.config.cache.get("bioimage_model", None)
+    if zip_file is not None and Path(zip_file).exists():
+        config = import_bioimage_model(zip_file)
+        assert isinstance(config, Configuration)
+    else:
+        raise ValueError("No bioimage model zip provided.")
