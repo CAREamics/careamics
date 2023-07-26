8f57e433601d94469a016fc1935ca115235b7b44
Mehdi Seifi <mehdiseifi@gmail.com>
2023-07-26 10:42:06 +0200

    change import test to use create_model

--- tests/bioimage/test_io.py
+++ tests/bioimage/test_io.py
@@ -6,8 +6,10 @@ import yaml
 import bioimageio.spec.shared.raw_nodes as nodes
 from bioimageio.core import load_resource_description
 
-from careamics_restoration.bioimage import import_bioimage_model
+# from careamics_restoration.bioimage import import_bioimage_model
 from careamics_restoration.engine import Engine
+from careamics_restoration.models import create_model
+from careamics_restoration.config import Configuration
 
 
 def test_bioimage_export_default(minimum_config: dict, tmp_path: Path, request):
@@ -51,9 +53,8 @@ def test_bioimage_export_default(minimum_config: dict, tmp_path: Path, request):
 def test_bioimage_import(request):
     zip_file = request.config.cache.get("bioimage_model", None)
     if zip_file is not None and Path(zip_file).exists():
-        checkpoint_path = import_bioimage_model(zip_file)
-        assert checkpoint_path.exists()
-        engine = Engine(model_path=checkpoint_path)
-        assert isinstance(engine, Engine)
+        # checkpoint_path = import_bioimage_model(zip_file)
+        model, optimizer, scheduler, scaler, config = create_model(model_path=zip_file)
+        assert isinstance(config, Configuration)
     else:
-        raise ValueError("No bioimage model zip provided.")
+        raise ValueError("No valid bioimage model zip provided.")
