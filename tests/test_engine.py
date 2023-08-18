69c236adb2ddc8e57d885d93b93986fb490f8e3c
CatEek <zubarev.ia@gmail.com>
2023-08-18 18:36:46 +0200

    save checkpoint test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -2,6 +2,7 @@ import pytest
 
 from careamics_restoration.config import Configuration
 from careamics_restoration.engine import Engine
+from careamics_restoration.models import create_model
 
 
 def test_engine_init_errors():
@@ -23,3 +24,18 @@ def test_engine_predict_errors(minimum_config: dict):
     config.data.std = None
     with pytest.raises(ValueError):
         engine.predict(external_input=None, pred_path="None")
+
+
+def test_engine_save_checkpoint(minimum_config: dict):
+    init_config = Configuration(**minimum_config)
+    engine = Engine(config=init_config)
+    path = engine.save_checkpoint(epoch=1, losses=[0.0], save_method='state_dict')
+    assert path.exists()
+
+    model, optimizer, scheduler, scaler, config = create_model(model_path=path)
+    assert model is not None
+    assert config is not None
+
+    assert config == init_config
+
+
