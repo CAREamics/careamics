00cdff78238e2ba55f978fbab457d075d9b96be8
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-10-05 12:15:24 +0200

    Fix some doc

--- src/careamics/dataset/prepare_dataset.py
+++ src/careamics/dataset/prepare_dataset.py
@@ -1,3 +1,8 @@
+"""
+Dataset preparation module.
+
+Methods to set up the datasets for training, validation and prediction.
+"""
 from pathlib import Path
 from typing import List, Optional, Union
 
@@ -12,27 +17,24 @@ from .tiff_dataset import TiffDataset
 def get_train_dataset(
     config: Configuration, train_path: str
 ) -> Union[TiffDataset, InMemoryDataset]:
-    """Create Dataset instance from configuration.
+    """
+    Create training dataset.
+
+    Depending on the configuration, this methods return either a TiffDataset or an
+    InMemoryDataset.
 
     Parameters
     ----------
     config : Configuration
-        Configuration object
+        Configuration.
     train_path : Union[str, Path]
-        Pathlike object with a path to training data
+        Path to training data.
 
     Returns
     -------
-        Dataset object
-
-    Raises
-    ------
-    ValueError
-        No training configuration found
+    Union[TiffDataset, InMemoryDataset]
+        Dataset.
     """
-    if config.training is None:
-        raise ValueError("Training configuration is not defined.")
-
     if config.data.in_memory:
         dataset = InMemoryDataset(
             data_path=train_path,
@@ -67,28 +69,23 @@ def get_train_dataset(
 
 
 def get_validation_dataset(config: Configuration, val_path: str) -> InMemoryDataset:
-    """Create Dataset instance from configuration.
+    """
+    Create validation dataset.
+
+    Validation dataset is kept in memory.
 
     Parameters
     ----------
     config : Configuration
-        Configuration object
+        Configuration.
     val_path : Union[str, Path]
-        Pathlike object with a path to validation data
+        Path to validation data.
 
     Returns
     -------
     TiffDataset
-        Dataset object
-
-    Raises
-    ------
-    ValueError
-        No validation configuration found
+        In memory dataset.
     """
-    if config.training is None:
-        raise ValueError("Training configuration is not defined.")
-
     data_path = val_path
 
     dataset = InMemoryDataset(
@@ -116,34 +113,35 @@ def get_prediction_dataset(
     overlaps: Optional[List[int]] = None,
     axes: Optional[str] = None,
 ) -> TiffDataset:
-    """Create Dataset instance from configuration.
+    """
+    Create prediction dataset.
 
     To use tiling, both `tile_shape` and `overlaps` must be specified, have same
     length, be divisible by 2 and greater than 0. Finally, the overlaps must be
     smaller than the tiles.
 
+    By default, axes are extracted from the configuration. To use images with
+    different axes, set the `axes` parameter. Note that the difference between
+    configuration and parameter axes must be S or T, but not any of the spatial
+    dimensions (e.g. 2D vs 3D).
+
     Parameters
     ----------
     config : Configuration
-        Configuration object
+        Configuration.
     pred_path : Union[str, Path]
-        Pathlike object with a path to prediction data
+        Path to prediction data.
     tile_shape : Optional[List[int]], optional
-        2D or 3D shape of the tiles to be predicted, by default None
+        2D or 3D shape of the tiles, by default None.
     overlaps : Optional[List[int]], optional
-        2D or 3D overlaps between tiles, by default None
+        2D or 3D overlaps between tiles, by default None.
     axes : Optional[str], optional
-        Axes of the data, by default None
+        Axes of the data, by default None.
 
     Returns
     -------
     TiffDataset
-        Dataset object
-
-    Raises
-    ------
-    ValueError
-
+        Dataset.
     """
     use_tiling = False  # default value
 
