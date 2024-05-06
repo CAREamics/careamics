63247c26497ecd5414de6d47f6da7d330c70ebcc
federico-carrara <federico1.carrara@mail.polimi.it>
2024-05-06 15:04:21 +0200

    Added FirstBottomUpBlock module

--- src/careamics/models/lvae/layers.py
+++ src/careamics/models/lvae/layers.py
@@ -1,6 +1,101 @@
 """
-Script containing the layers (nn.Module) used by the LadderVAE architecture.
+Script containing the common layers (nn.Module) reused by the LadderVAE architecture.
 """
+import torch
+import torch.nn as nn
+
+
+
+class ResidualBlock(nn.Module):
+    """
+    Residual block with 2 convolutional layers.
+    Input, intermediate, and output channels are the same. Padding is always
+    'same'. The 2 convolutional layers have the same groups. No stride allowed,
+    and kernel sizes have to be odd.
+    The result is:
+        out = gate(f(x)) + x
+    where an argument controls the presence of the gating mechanism, and f(x)
+    has different structures depending on the argument block_type.
+    block_type is a string specifying the structure of the block, where:
+        a = activation
+        b = batch norm
+        c = conv layer
+        d = dropout.
+    For example, bacdbacd has 2x (batchnorm, activation, conv, dropout).
+    """
+
+    default_kernel_size = (3, 3)
+
+    def __init__(self,
+                 channels: int,
+                 nonlin,
+                 kernel=None,
+                 groups=1,
+                 batchnorm: bool = True,
+                 block_type: str = None,
+                 dropout=None,
+                 gated=None,
+                 skip_padding=False,
+                 conv2d_bias=True):
+        super().__init__()
+        if kernel is None:
+            kernel = self.default_kernel_size
+        elif isinstance(kernel, int):
+            kernel = (kernel, kernel)
+        elif len(kernel) != 2:
+            raise ValueError("kernel has to be None, int, or an iterable of length 2")
+        assert all([k % 2 == 1 for k in kernel]), "kernel sizes have to be odd"
+        kernel = list(kernel)
+        self.skip_padding = skip_padding
+        pad = [0] * len(kernel) if self.skip_padding else [k // 2 for k in kernel]
+        print(kernel, pad)
+        self.gated = gated
+        modules = []
+
+        if block_type == 'cabdcabd':
+            for i in range(2):
+                conv = nn.Conv2d(channels, channels, kernel[i], padding=pad[i], groups=groups, bias=conv2d_bias)
+                modules.append(conv)
+                modules.append(nonlin())
+                if batchnorm:
+                    modules.append(nn.BatchNorm2d(channels))
+                if dropout is not None:
+                    modules.append(nn.Dropout2d(dropout))
+
+        elif block_type == 'bacdbac':
+            for i in range(2):
+                if batchnorm:
+                    modules.append(nn.BatchNorm2d(channels))
+                modules.append(nonlin())
+                conv = nn.Conv2d(channels, channels, kernel[i], padding=pad[i], groups=groups, bias=conv2d_bias)
+                modules.append(conv)
+                if dropout is not None and i == 0:
+                    modules.append(nn.Dropout2d(dropout))
+
+        elif block_type == 'bacdbacd':
+            for i in range(2):
+                if batchnorm:
+                    modules.append(nn.BatchNorm2d(channels))
+                modules.append(nonlin())
+                conv = nn.Conv2d(channels, channels, kernel[i], padding=pad[i], groups=groups, bias=conv2d_bias)
+                modules.append(conv)
+                modules.append(nn.Dropout2d(dropout))
+
+        else:
+            raise ValueError("unrecognized block type '{}'".format(block_type))
+
+        # if gated:
+        #     modules.append(GateLayer2d(channels, 1, nonlin))
+        # self.block = nn.Sequential(*modules)
+
+    def forward(self, x):
+
+        out = self.block(x)
+        if out.shape != x.shape:
+            return out + F.center_crop(x, out.shape[-2:])
+        else:
+            return out + x
+
 
 class ResBlockWithResampling(nn.Module):
     """
