4c6a8b28d89c719b683129632542b4d97f9d0546
federico-carrara <federico1.carrara@mail.polimi.it>
2024-05-08 17:48:26 +0200

    Added crop_img_tensor() and _pad_crop_img() methods

--- src/careamics/models/lvae/utils.py
+++ src/careamics/models/lvae/utils.py
@@ -1,6 +1,8 @@
 """
 Script for utility functions needed by the LVAE model.
 """
+import torch
+import torch.nn as nn 
 
 def torch_nanmean(inp):
     return torch.mean(inp[~inp.isnan()])
@@ -62,3 +64,48 @@ class LossType(Enum):
     ElboCL = 6
     ElboRestrictedReconstruction = 7
     DenoiSplitMuSplit = 8
+    
+
+def crop_img_tensor(x, size) -> torch.Tensor:
+    """Crops a tensor.
+    Crops a tensor of shape (batch, channels, h, w) to new height and width
+    given by a tuple.
+    Args:
+        x (torch.Tensor): Input image
+        size (list or tuple): Desired size (height, width)
+    Returns:
+        The cropped tensor
+    """
+    return _pad_crop_img(x, size, 'crop')
+
+
+def _pad_crop_img(x, size, mode) -> torch.Tensor:
+    """ Pads or crops a tensor.
+    Pads or crops a tensor of shape (batch, channels, h, w) to new height
+    and width given by a tuple.
+    Args:
+        x (torch.Tensor): Input image
+        size (list or tuple): Desired size (height, width)
+        mode (str): Mode, either 'pad' or 'crop'
+    Returns:
+        The padded or cropped tensor
+    """
+
+    assert x.dim() == 4 and len(size) == 2
+    size = tuple(size)
+    x_size = x.size()[2:4]
+    if mode == 'pad':
+        cond = x_size[0] > size[0] or x_size[1] > size[1]
+    elif mode == 'crop':
+        cond = x_size[0] < size[0] or x_size[1] < size[1]
+    else:
+        raise ValueError("invalid mode '{}'".format(mode))
+    if cond:
+        raise ValueError('trying to {} from size {} to size {}'.format(mode, x_size, size))
+    dr, dc = (abs(x_size[0] - size[0]), abs(x_size[1] - size[1]))
+    dr1, dr2 = dr // 2, dr - (dr // 2)
+    dc1, dc2 = dc // 2, dc - (dc // 2)
+    if mode == 'pad':
+        return nn.functional.pad(x, [dc1, dc2, dr1, dr2, 0, 0, 0, 0])
+    elif mode == 'crop':
+        return x[:, :, dr1:x_size[0] - dr2, dc1:x_size[1] - dc2]
