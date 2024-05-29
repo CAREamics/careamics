0fbdf025afd63e9e05cc2661b7d76c68b1087b3e
melisande-c <milly.croft@gmail.com>
2024-05-29 17:27:56 +0200

    remove(hydra cli): delete all hydra cli files; too complicated to combine with pydantic models

--- src/careamics/cli_hydra/main.py
+++
@@ -1,8 +0,0 @@
-from omegaconf import DictConfig, OmegaConf
-import hydra
-
-@hydra.main(version_base=None, config_path="conf", config_name="config")
-def main(cfg: DictConfig) -> None:
-    print(OmegaConf.missing_keys(cfg))
-    print(OmegaConf.resolve(cfg))
-    print(OmegaConf.to_yaml(cfg))
