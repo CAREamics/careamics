fcbedf67a5927bb140c8a6330bbb0a7c56794904
CatEek <zubarev.ia@gmail.com>
2024-06-03 18:23:23 +0200

    config tests fix

--- tests/config/transformations/test_normalize_model.py
+++ tests/config/transformations/test_normalize_model.py
@@ -1,13 +1,35 @@
 from careamics.config.transformations import NormalizeModel
 
 
-def test_setting_mean_std():
-    """Test that we can set the mean and std values."""
-    model = NormalizeModel(name="Normalize", mean=0.5, std=0.5)
-    assert model.mean == 0.5
-    assert model.std == 0.5
+def test_setting_image_means_std():
+    """Test that we can set the image_means and std values."""
+    model = NormalizeModel(name="Normalize", image_means=[0.5], image_stds=[0.5])
+    assert model.image_means == [0.5]
+    assert model.image_stds == [0.5]
 
-    model.mean = 0.6
-    model.std = 0.6
-    assert model.mean == 0.6
-    assert model.std == 0.6
+    model.image_means = [0.6]
+    model.image_stds = [0.6]
+    assert model.image_means == [0.6]
+    assert model.image_stds == [0.6]
+
+    model = NormalizeModel(
+        name="Normalize",
+        image_means=[0.5],
+        image_stds=[0.5],
+        target_means=[0.5],
+        target_stds=[0.5],
+    )
+    assert model.image_means == [0.5]
+    assert model.image_stds == [0.5]
+    assert model.target_means == [0.5]
+    assert model.target_stds == [0.5]
+
+    model.image_means = [0.6]
+    model.image_stds = [0.6]
+    model.target_means = [0.6]
+    model.target_stds = [0.6]
+
+    assert model.image_means == [0.6]
+    assert model.image_stds == [0.6]
+    assert model.target_means == [0.6]
+    assert model.target_stds == [0.6]
