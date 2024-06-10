91ca11f39737be7d287d3c7bf3e7caae3b6d7c85
federico-carrara <federico1.carrara@mail.polimi.it>
2024-06-10 11:51:32 +0200

    Renamed EvalOnWholeFrames.ipynb to evaluate_LVAE.ipynb + Added and adapted code for plotting comparison between ground truth and prediction and to compute associated metrics + added required helper functions in eval_utils.py and metrics.py

--- src/careamics/models/lvae/eval_utils.py
+++ src/careamics/models/lvae/eval_utils.py
@@ -11,6 +11,7 @@ from typing import Dict, List, Union, Literal
 
 import torch
 import numpy as np 
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec    
 from torch.utils.data import DataLoader
@@ -19,6 +20,8 @@ from tqdm import tqdm
 from .metrics import RangeInvariantPsnr, RunningPSNR
 from .utils import ModelType, LossType
 
+# ------------------------------------------------------------------------------------------------
+# Function of plotting: TODO -> moved them to another file, plot_utils.py
 def clean_ax(ax):
     """
     Helper function to remove ticks from axes in plots.
@@ -90,47 +93,6 @@ def get_first_index(bin_count, quantile):
         if normalized_cumsum[i] > quantile:
             return i
     return None
-
-
-def get_predictions(
-    idx, 
-    val_dset, 
-    model, 
-    mmse_count=50, 
-    patch_size=256
-):
-    """
-    Given an index and a validation/test set, it returns the input, target and the reconstructed images for that index.
-    """
-    print(f'Predicting for {idx}')
-    val_dset.set_img_sz(patch_size, 64)
-
-    with torch.no_grad():
-        # val_dset.enable_noise()
-        inp, tar = val_dset[idx]
-        # val_dset.disable_noise()
-
-        inp = torch.Tensor(inp[None])
-        tar = torch.Tensor(tar[None])
-        inp = inp.cuda()
-        x_normalized = model.normalize_input(inp)
-        tar = tar.cuda()
-        tar_normalized = model.normalize_target(tar)
-
-        recon_img_list = []
-        for _ in range(mmse_count):
-            recon_normalized, td_data = model(x_normalized)
-            rec_loss, imgs = model.get_reconstruction_loss(
-                recon_normalized,
-                x_normalized,
-                tar_normalized,
-                return_predicted_img=True
-            )
-            imgs = model.unnormalize_target(imgs)
-            recon_img_list.append(imgs.cpu().numpy()[0])
-
-    recon_img_list = np.array(recon_img_list)
-    return inp, tar, recon_img_list
 
 
 def show_for_one(
@@ -365,7 +327,141 @@ def plot_calibration(ax, calibration_stats):
     ax.set_xlabel('RMV')
     ax.set_ylabel('RMSE')
     ax.legend()
-    
+
+
+def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
+    '''
+    Adapted from https://stackoverflow.com/questions/7404116/defining-the-midpoint-of-a-colormap-in-matplotlib
+
+    Function to offset the "center" of a colormap. Useful for
+    data with a negative min and positive max and you want the
+    middle of the colormap's dynamic range to be at zero.
+
+    Input
+    -----
+      cmap : The matplotlib colormap to be altered
+      start : Offset from lowest point in the colormap's range.
+          Defaults to 0.0 (no lower offset). Should be between
+          0.0 and `midpoint`.
+      midpoint : The new center of the colormap. Defaults to 
+          0.5 (no shift). Should be between 0.0 and 1.0. In
+          general, this should be  1 - vmax / (vmax + abs(vmin))
+          For example if your data range from -15.0 to +5.0 and
+          you want the center of the colormap at 0.0, `midpoint`
+          should be set to  1 - 5/(5 + 15)) or 0.75
+      stop : Offset from highest point in the colormap's range.
+          Defaults to 1.0 (no upper offset). Should be between
+          `midpoint` and 1.0.
+    '''
+    cdict = {'red': [], 'green': [], 'blue': [], 'alpha': []}
+
+    # regular index to compute the colors
+    reg_index = np.linspace(start, stop, 257)
+    mid_idx = len(reg_index) // 2
+    # shifted index to match the data
+    shift_index = np.hstack(
+        [np.linspace(0.0, midpoint, 128, endpoint=False),
+         np.linspace(midpoint, 1.0, 129, endpoint=True)])
+
+    for ri, si in zip(reg_index, shift_index):
+        r, g, b, a = cmap(ri)
+        a = np.abs(ri - reg_index[mid_idx]) / reg_index[mid_idx]
+        # print(a)
+        cdict['red'].append((si, r, r))
+        cdict['green'].append((si, g, g))
+        cdict['blue'].append((si, b, b))
+        cdict['alpha'].append((si, a, a))
+
+    newcmap = matplotlib.colors.LinearSegmentedColormap(name, cdict)
+    matplotlib.colormaps.register(cmap=newcmap, force=True)
+
+    return newcmap
+
+
+def get_fractional_change(target, prediction, max_val=None):
+    """
+    Get relative difference between target and prediction.
+    """
+    if max_val is None:
+        max_val = target.max()
+    return (target - prediction) / max_val
+
+
+def get_zero_centered_midval(error):
+    """
+    When done this way, the midval ensures that the colorbar is centered at 0. (Don't know how, but it works ;))
+    """
+    vmax = error.max()
+    vmin = error.min()
+    midval = 1 - vmax / (vmax + abs(vmin))
+    return midval
+
+
+def plot_error(
+    target, 
+    prediction, 
+    cmap=matplotlib.cm.coolwarm, 
+    ax=None, 
+    max_val=None
+):
+    """
+    Plot the relative difference between target and prediction.
+    NOTE: The plot is overlapped to the prediction image (in gray scale).
+    NOTE: The colorbar is centered at 0.
+    """
+    if ax is None:
+        _, ax = plt.subplots(figsize=(6, 6))
+
+    # Relative difference between target and prediction
+    rel_diff = get_fractional_change(target, prediction, max_val=max_val)
+    midval = get_zero_centered_midval(rel_diff)
+    shifted_cmap = shiftedColorMap(cmap, start=0, midpoint=midval, stop=1.0, name='shiftedcmap')
+    ax.imshow(prediction, cmap='gray')
+    img_err = ax.imshow(rel_diff, cmap=shifted_cmap, alpha=1)
+    plt.colorbar(img_err, ax=ax)
+# ------------------------------------------------------------------------------------------------
+
+def get_predictions(
+    idx, 
+    val_dset, 
+    model, 
+    mmse_count=50, 
+    patch_size=256
+):
+    """
+    Given an index and a validation/test set, it returns the input, target and the reconstructed images for that index.
+    """
+    print(f'Predicting for {idx}')
+    val_dset.set_img_sz(patch_size, 64)
+
+    with torch.no_grad():
+        # val_dset.enable_noise()
+        inp, tar = val_dset[idx]
+        # val_dset.disable_noise()
+
+        inp = torch.Tensor(inp[None])
+        tar = torch.Tensor(tar[None])
+        inp = inp.cuda()
+        x_normalized = model.normalize_input(inp)
+        tar = tar.cuda()
+        tar_normalized = model.normalize_target(tar)
+
+        recon_img_list = []
+        for _ in range(mmse_count):
+            recon_normalized, td_data = model(x_normalized)
+            rec_loss, imgs = model.get_reconstruction_loss(
+                recon_normalized,
+                x_normalized,
+                tar_normalized,
+                return_predicted_img=True
+            )
+            imgs = model.unnormalize_target(imgs)
+            recon_img_list.append(imgs.cpu().numpy()[0])
+
+    recon_img_list = np.array(recon_img_list)
+    return inp, tar, recon_img_list
+
+
 
 def get_dset_predictions(
     model, 
@@ -462,6 +558,7 @@ def get_dset_predictions(
         psnr, 
         np.concatenate(predictions_std, axis=0)
     )
+    
 
 # ------------------------------------------------------------------------------------------
 ### Classes and Functions used to stitch predictions
--- src/careamics/models/lvae/metrics.py
+++ src/careamics/models/lvae/metrics.py
@@ -4,6 +4,7 @@ This script contains the functions/classes to compute loss and metrics used to t
 import torch
 import numpy as np
 from skimage.metrics import structural_similarity
+from torchmetrics.image import MultiScaleStructuralSimilarityIndexMeasure
 
 from .utils import allow_numpy
 
@@ -168,3 +169,45 @@ def compute_masked_psnr(mask, tar1, tar2, pred1, pred2):
 def avg_ssim(target, prediction):
     ssim = [structural_similarity(target[i],prediction[i], data_range=(target[i].max() - target[i].min())) for i in range(len(target))]
     return np.mean(ssim),np.std(ssim)
+
+@allow_numpy
+def range_invariant_multiscale_ssim(gt_, pred_):
+    """
+    Computes range invariant multiscale ssim for one channel.
+    This has the benefit that it is invariant to scalar multiplications in the prediction.
+    """
+
+    shape = gt_.shape
+    gt_ = torch.Tensor(gt_.reshape((shape[0],-1)))
+    pred_ = torch.Tensor(pred_.reshape((shape[0],-1)))
+    gt_ = zero_mean(gt_)
+    pred_ = zero_mean(pred_)
+    pred_ = fix(gt_, pred_)
+    pred_ = pred_.reshape(shape)
+    gt_ = gt_.reshape(shape)
+    
+    ms_ssim = MultiScaleStructuralSimilarityIndexMeasure(data_range=gt_.max() - gt_.min())
+    return ms_ssim(torch.Tensor(pred_[:, None]), torch.Tensor(gt_[:, None])).item()
+
+
+def compute_multiscale_ssim(gt_, pred_, range_invariant=True):
+    """
+    Computes multiscale ssim for each channel.
+    Args:
+    gt_: ground truth image with shape (N, H, W, C)
+    pred_: predicted image with shape (N, H, W, C)
+    range_invariant: whether to use range invariant multiscale ssim
+    """
+    ms_ssim_values = {i: None for i in range(gt_.shape[-1])}
+    for ch_idx in range(gt_.shape[-1]):
+        tar_tmp = gt_[..., ch_idx]
+        pred_tmp = pred_[..., ch_idx]
+        if range_invariant:
+            ms_ssim_values[ch_idx] = range_invariant_multiscale_ssim(tar_tmp, pred_tmp)
+        else:
+            ms_ssim = MultiScaleStructuralSimilarityIndexMeasure(data_range=tar_tmp.max() - tar_tmp.min())
+            ms_ssim_values[ch_idx] = ms_ssim(torch.Tensor(pred_tmp[:, None]), torch.Tensor(tar_tmp[:, None])).item()
+
+    output = [ms_ssim_values[i] for i in range(gt_.shape[-1])]
+    return output
+
