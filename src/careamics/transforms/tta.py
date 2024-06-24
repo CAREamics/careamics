170ebe6d916f50c77358deadc03ff39edb5bcc52
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2024-06-24 14:35:28 +0200

    Fix: Fix TTA and add tests (#164)
    ### Description
    
    Following https://github.com/CAREamics/careamics/issues/157, this PR
    fixes the axes for the flipping and add tests.
    
    - **What**: Fixing test-time augmentation used during the prediction
    step.
    - **Why**: The axes of some of the transformations were wrong.
    - **How**: (Almost-)Explicitly write the augmentations and add tests.
    
    ### Changes Made
    
    - **Added**: `test_tta.py`.
    - **Modified**: `CAREamist.predict` tta explanations,
    `ImageRestorationTTA`.
    
    ### Related Issues
    
    - Fixes https://github.com/CAREamics/careamics/issues/157
    
    ---
    
    **Please ensure your PR meets the following requirements:**
    
    - [x] Code builds and passes tests locally, including doctests
    - [x] New tests have been added (for bug fixes/features)
    - [x] Pre-commit passes
    - [ ] PR to the documentation exists (for bug fixes / features)

--- src/careamics/transforms/tta.py
+++ src/careamics/transforms/tta.py
@@ -1,11 +1,8 @@
 """Test-time augmentations."""
 
-from typing import List
-
 from torch import Tensor, flip, mean, rot90, stack
 
 
-# TODO add tests
 class ImageRestorationTTA:
     """
     Test-time augmentation for image restoration tasks.
@@ -13,62 +10,79 @@ class ImageRestorationTTA:
     The augmentation is performed using all 90 deg rotations and their flipped version,
     as well as the original image flipped.
 
-    Tensors should be of shape SC(Z)YX
+    Tensors should be of shape SC(Z)YX.
 
     This transformation is used in the LightningModule in order to perform test-time
-    agumentation.
+    augmentation.
     """
 
-    def __init__(self) -> None:
-        """Constructor."""
-        pass
-
-    def forward(self, x: Tensor) -> List[Tensor]:
+    def forward(self, input_tensor: Tensor) -> list[Tensor]:
         """
         Apply test-time augmentation to the input tensor.
 
         Parameters
         ----------
-        x : Tensor
+        input_tensor : Tensor
             Input tensor, shape SC(Z)YX.
 
         Returns
         -------
-        List[Tensor]
+        list of torch.Tensor
             List of augmented tensors.
         """
-        augmented = [
-            x,
-            rot90(x, 1, dims=(-2, -1)),
-            rot90(x, 2, dims=(-2, -1)),
-            rot90(x, 3, dims=(-2, -1)),
-        ]
-        augmented_flip = augmented.copy()
-        for x_ in augmented:
-            augmented_flip.append(flip(x_, dims=(-3, -1)))
-        return augmented_flip
+        # axes: only applies to YX axes
+        axes = (-2, -1)
 
-    def backward(self, x: List[Tensor]) -> Tensor:
+        augmented = [
+            # original
+            input_tensor,
+            # rotations
+            rot90(input_tensor, 1, dims=axes),
+            rot90(input_tensor, 2, dims=axes),
+            rot90(input_tensor, 3, dims=axes),
+            # original flipped
+            flip(input_tensor, dims=(axes[0],)),
+            flip(input_tensor, dims=(axes[1],)),
+        ]
+
+        # rotated once, flipped
+        augmented.extend(
+            [
+                flip(augmented[1], dims=(axes[0],)),
+                flip(augmented[1], dims=(axes[1],)),
+            ]
+        )
+
+        return augmented
+
+    def backward(self, x: list[Tensor]) -> Tensor:
         """Undo the test-time augmentation.
 
         Parameters
         ----------
         x : Any
-            List of augmented tensors.
+            List of augmented tensors of shape SC(Z)YX.
 
         Returns
         -------
         Any
             Original tensor.
         """
+        axes = (-2, -1)
+
         reverse = [
+            # original
             x[0],
-            rot90(x[1], -1, dims=(-2, -1)),
-            rot90(x[2], -2, dims=(-2, -1)),
-            rot90(x[3], -3, dims=(-2, -1)),
-            flip(x[4], dims=(-3, -1)),
-            rot90(flip(x[5], dims=(-3, -1)), -1, dims=(-2, -1)),
-            rot90(flip(x[6], dims=(-3, -1)), -2, dims=(-2, -1)),
-            rot90(flip(x[7], dims=(-3, -1)), -3, dims=(-2, -1)),
+            # rotated
+            rot90(x[1], -1, dims=axes),
+            rot90(x[2], -2, dims=axes),
+            rot90(x[3], -3, dims=axes),
+            # original flipped
+            flip(x[4], dims=(axes[0],)),
+            flip(x[5], dims=(axes[1],)),
+            # rotated once, flipped
+            rot90(flip(x[6], dims=(axes[0],)), -1, dims=axes),
+            rot90(flip(x[7], dims=(axes[1],)), -1, dims=axes),
         ]
+
         return mean(stack(reverse), dim=0)
