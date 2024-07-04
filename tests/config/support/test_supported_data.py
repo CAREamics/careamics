e15b6cd37da92d9b3c8bfe27a10ef636e0c7a8b6
melisande-c <milly.croft@gmail.com>
2024-07-04 14:16:59 +0200

    test(supported data): rename extension pattern tests\nto mirror renaming of method

--- tests/config/support/test_supported_data.py
+++ tests/config/support/test_supported_data.py
@@ -8,7 +8,7 @@ import tifffile
 from careamics.config.support import SupportedData
 
 
-def test_extension_tiff_fnmatch(tmp_path: Path):
+def test_extension_pattern_tiff_fnmatch(tmp_path: Path):
     """Test that the TIFF extension is compatible with fnmatch."""
     path = tmp_path / "test.tif"
 
@@ -19,7 +19,7 @@ def test_extension_tiff_fnmatch(tmp_path: Path):
     assert fnmatch(path, SupportedData.get_extension_pattern(SupportedData.TIFF))
 
 
-def test_extension_tiff_rglob(tmp_path: Path):
+def test_extension_pattern_tiff_rglob(tmp_path: Path):
     """Test that the TIFF extension is compatible with Path.rglob."""
     # create text file
     text_path = tmp_path / "test.txt"
@@ -38,7 +38,7 @@ def test_extension_tiff_rglob(tmp_path: Path):
     assert files[0] == path
 
 
-def test_extension_custom_fnmatch(tmp_path: Path):
+def test_extension_pattern_custom_fnmatch(tmp_path: Path):
     """Test that the custom extension is compatible with fnmatch."""
     path = tmp_path / "test.czi"
 
@@ -49,7 +49,7 @@ def test_extension_custom_fnmatch(tmp_path: Path):
     assert fnmatch(path, SupportedData.get_extension_pattern(SupportedData.CUSTOM))
 
 
-def test_extension_custom_rglob(tmp_path: Path):
+def test_extension_pattern_custom_rglob(tmp_path: Path):
     """Test that the custom extension is compatible with Path.rglob."""
     # create text file
     text_path = tmp_path / "test.txt"
@@ -68,13 +68,14 @@ def test_extension_custom_rglob(tmp_path: Path):
     assert set(files) == {path, text_path}
 
 
-def test_extension_array_error():
+def test_extension_pattern_array_error():
     """Test that the array extension raises NotImplementedError."""
     with pytest.raises(NotImplementedError):
         SupportedData.get_extension_pattern(SupportedData.ARRAY)
 
 
-def test_extension_any_error():
+def test_extension_pattern_any_error():
     """Test that any extension raises NotImplementedError."""
     with pytest.raises(ValueError):
         SupportedData.get_extension_pattern("some random")
+
