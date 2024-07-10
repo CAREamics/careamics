c71528f7de04cb7a1785a70cc0de64c01ffbda48
melisande-c <milly.croft@gmail.com>
2024-07-10 11:40:12 +0200

    docs(write_strategy_factory): add

--- src/careamics/lightning/callbacks/prediction_writer_callback/write_strategy_factory.py
+++ src/careamics/lightning/callbacks/prediction_writer_callback/write_strategy_factory.py
@@ -15,7 +15,41 @@ def create_write_strategy(
     write_extension: Optional[str] = None,
     write_func_kwargs: Optional[dict[str, Any]] = None,
 ) -> WriteStrategy:
+    """
+    Create a write strategy from convinient parameters.
 
+    Parameters
+    ----------
+    write_type : SupportedData or str
+        The data type to save as, includes custom.
+    tiled : bool
+        Whether the prediction will be tiled or not.
+    write_func : WriteFunc, optional
+        If a known `write_type` is selected this argument is ignored. For a custom
+        `write_type` a function to save the data must be passed. See notes below.
+    write_extension : str, optional
+        If a known `write_type` is selected this argument is ignored. For a custom
+        `write_type` an extension to save the data with must be passed.
+    write_func_kwargs : dict of {str: any}, optional
+        Additional keyword arguments to be passed to the save function.
+
+    Returns
+    -------
+    WriteStrategy
+        _description_
+
+    Notes
+    -----
+    The `write_func` function signature must match that of the example below
+        ```
+        write_func(file_path: Path, img: NDArray, *args, **kwargs) -> None: ...
+        ```
+
+    The `write_func_kwargs` will be passed to the `write_func` doing the following:
+        ```
+        write_func(file_path=file_path, img=img, **kwargs)
+        ```
+    """
     if not tiled:
         write_strategy = WriteImage(
             write_func=write_func,
@@ -40,24 +74,85 @@ def _create_tiled_write_strategy(
     write_extension: Optional[str] = None,
     write_func_kwargs: Optional[dict[str, Any]] = None,
 ) -> WriteStrategy:
+    """
+    Create a tiled write strategy. 
+    
+    Either `CacheTiles` for caching tiles until a whole image is predicted or 
+    `WriteTilesZarr` for writing tiles directly to disk.
+
+    Parameters
+    ----------
+    write_type : SupportedData or str
+        The data type to save as, includes custom.
+    write_func : WriteFunc, optional
+        If a known `write_type` is selected this argument is ignored. For a custom
+        `write_type` a function to save the data must be passed. See notes below.
+    write_extension : str, optional
+        If a known `write_type` is selected this argument is ignored. For a custom
+        `write_type` an extension to save the data with must be passed.
+    write_func_kwargs : dict of {str: any}, optional
+        Additional keyword arguments to be passed to the save function.
+
+    Returns
+    -------
+    WriteStrategy
+        A strategy for writing tiled predictions.
+
+    Raises
+    ------
+    NotImplementedError
+        if `write_type="zarr" is chosen.
+    """
     # if write_type == SupportedData.ZARR:
     #    create *args, **kwargs
     #    return WriteTilesZarr(*args, **kwargs)
     # else:
-    write_func = select_write_func(write_type=write_type, write_func=write_func)
-    write_extension = select_write_extension(
-        write_type=write_type, write_func=write_func
-    )
-    return CacheTiles(
-        write_func=write_func,
-        write_extension=write_extension,
-        write_func_kwargs=write_func_kwargs,
-    )
+    if write_type == "zarr":
+        raise NotImplementedError("Saving to zarr is not implemented yet.")
+    else:
+        write_func = select_write_func(write_type=write_type, write_func=write_func)
+        write_extension = select_write_extension(
+            write_type=write_type, write_func=write_func
+        )
+        return CacheTiles(
+            write_func=write_func,
+            write_extension=write_extension,
+            write_func_kwargs=write_func_kwargs,
+        )
 
 
 def select_write_func(
     write_type: Union[SupportedData, str], write_func: Optional[WriteFunc] = None
 ) -> WriteFunc:
+    """
+    Return a function to write images. If `write_type` is "custom" then `write_func`,
+    otherwise the known write function is selected.
+
+    Parameters
+    ----------
+    write_type : SupportedData or str
+        The data type to save as, includes custom.
+    write_func : WriteFunc, optional
+        If a known `write_type` is selected this argument is ignored. For a custom
+        `write_type` a function to save the data must be passed. See notes below.
+
+    Returns
+    -------
+    WriteFunc
+        A function for writing images.
+
+    Raises
+    ------
+    ValueError
+        If `write_type="custom"` but `write_func` has not been given.
+
+    Notes
+    -----
+    The `write_func` function signature must match that of the example below
+        ```
+        write_func(file_path: Path, img: NDArray, *args, **kwargs) -> None: ...
+        ```
+    """
     if write_type == SupportedData.CUSTOM:
         if write_func is None:
             raise ValueError(
@@ -73,6 +168,28 @@ def select_write_func(
 def select_write_extension(
     write_type: Union[SupportedData, str], write_extension: Optional[str] = None
 ) -> str:
+    """
+    Return an extension to add to file paths. If `write_type` is "custom" then 
+    `write_extension`, otherwise the known write extension is selected.
+
+    Parameters
+    ----------
+    write_type : SupportedData or str
+        The data type to save as, includes custom.
+    write_extension : str, optional
+        If a known `write_type` is selected this argument is ignored. For a custom
+        `write_type` an extension to save the data with must be passed.
+
+    Returns
+    -------
+    str
+        The extension to be added to file paths.
+
+    Raises
+    ------
+    ValueError
+        If `self.save_type="custom"` but `save_extension` has not been given.
+    """
     if write_type == SupportedData.CUSTOM:
         if write_extension is None:
             raise ValueError("A save extension must be provided for custom data types.")
