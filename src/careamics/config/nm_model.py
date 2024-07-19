56621d54d318d43066f8de5213bb96f7ba9e2998
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-07-19 13:02:23 +0200

    fix: solve pydantic warning

--- src/careamics/config/nm_model.py
+++ src/careamics/config/nm_model.py
@@ -8,9 +8,13 @@ from pydantic import BaseModel, ConfigDict, Field
 class GaussianMixtureNoiseModel(BaseModel):
     """Gaussian mixture noise model."""
 
-    model_config = ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
+    model_config = ConfigDict(
+        validate_assignment=True,
+        arbitrary_types_allowed=True,
+        protected_namespaces=(),  # to allow using model_type as a name
+    )
     # TODO What are all these parameters?
-    model_type: Literal["GaussianMixtureNoiseModel"]
+    model_type: Literal["GaussianMixtureNoiseModel"]  # TODO consider changing name
     weight: Any = None  # TODO wtf ?
     n_gaussian: int = Field(default=1, ge=1)
     n_coeff: int = Field(default=2, ge=2)
