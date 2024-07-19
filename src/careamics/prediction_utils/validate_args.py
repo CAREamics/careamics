76bf897c9eb7662cca3ef4afb43baaadf9bbfbbf
melisande-c <milly.croft@gmail.com>
2024-07-19 17:24:17 +0200

    docs(validate_unet_tile_size): add docstring

--- src/careamics/prediction_utils/validate_args.py
+++ src/careamics/prediction_utils/validate_args.py
@@ -2,6 +2,21 @@
 
 
 def validate_unet_tile_size(model_depth: int, tile_size: tuple[int, ...]):
+    """
+    Validate that `tile_size` is equal to `k*2^depth` where `k` is an integer.
+
+    Parameters
+    ----------
+    model_depth : int
+        The depth of the UNet model.
+    tile_size : tuple of int
+        The tile size.
+
+    Raises
+    ------
+    ValueError
+        If `tile_size` is not equal to `k*2^depth` where `k` is an integer.
+    """
     # tile size must be equal to k*2^n,
     # where n is the number of pooling layers (equal to the depth) and k is an integer
     tile_increment = 2**model_depth
