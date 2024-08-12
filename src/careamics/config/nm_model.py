3028a82934ab84d4790192a5aae449d7f50afad5
federico-carrara <federico1.carrara@mail.polimi.it>
2024-08-12 12:27:25 +0200

    refac: removed deprecated TODOs

--- src/careamics/config/nm_model.py
+++ src/careamics/config/nm_model.py
@@ -12,16 +12,15 @@ class GaussianMixtureNmModel(BaseModel):
     model_config = ConfigDict(
         validate_assignment=True, arbitrary_types_allowed=True, extra="allow"
     )
-    # TODO What are all these parameters?
-    model_type: Literal["GaussianMixtureNoiseModel"]
+    model_type: Literal["GaussianMixtureNoiseModel"] # TODO: why do we need this? 
     path: Union[Path, str, None] = None
-    weight: Any = None  # TODO wtf ?
+    weight: Any = None
     n_gaussian: int = Field(default=1, ge=1)
     n_coeff: int = Field(default=2, ge=2)
     min_signal: float = Field(default=0.0, ge=0.0)
     max_signal: float = Field(default=1.0, ge=0.0)
     min_sigma: Any = None
-    tol: float = Field(default=1e-10)  # TODO whatever the fuck this is
+    tol: float = Field(default=1e-10)
 
 # The noise model is given by a set of GMMs, one for each target
 # e.g., 2 target channels, 2 noise models
