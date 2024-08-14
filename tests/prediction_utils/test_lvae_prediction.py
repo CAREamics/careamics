1673bd24ddb66c247672036ea0db975832404463
melisande-c <milly.croft@gmail.com>
2024-08-14 12:33:51 +0200

    test: correct lvae prediction dimensions

---
+++ tests/prediction_utils/test_lvae_prediction.py
@@ -0,0 +1,80 @@
+import pytest
+import torch
+
+from careamics.models.lvae.lvae import LadderVAE
+from careamics.prediction_utils.lvae_prediction import (
+    lvae_predict_mmse,
+    lvae_predict_single_sample,
+)
+
+
+@pytest.fixture
+def minimum_lvae_params():
+    return {
+        "input_shape": 64,
+        "output_channels": 2,
+        "multiscale_count": None,
+        "z_dims": [128, 128, 128, 128],
+        "encoder_n_filters": 64,
+        "decoder_n_filters": 64,
+        "encoder_dropout": 0.1,
+        "decoder_dropout": 0.1,
+        "nonlinearity": "ELU",
+        "predict_logvar": "pixelwise",
+        "enable_noise_model": False,
+        "analytical_kl": False,
+    }
+
+
+@pytest.mark.parametrize("predict_logvar", ["pixelwise", None])
+@pytest.mark.parametrize("output_channels", [2, 3])
+def test_lvae_predict_single_sample(
+    minimum_lvae_params, predict_logvar, output_channels
+):
+    """Test predictions of a single sample."""
+    minimum_lvae_params["predict_logvar"] = predict_logvar
+    minimum_lvae_params["output_channels"] = output_channels
+
+    input_shape = minimum_lvae_params["input_shape"]
+
+    # initialize model
+    model = LadderVAE(**minimum_lvae_params)
+    # initialize likelihoods (commented out of __init__ atm?)
+    model.create_likelihood_module()
+
+    # dummy input
+    x = torch.rand(size=(1, 1, input_shape, input_shape))
+    # prediction
+    y, log_var = lvae_predict_single_sample(model, x)
+
+    assert y.shape == (1, output_channels, input_shape, input_shape)
+    if predict_logvar == "pixelwise":
+        assert log_var.shape == (1, output_channels, input_shape, input_shape)
+    elif predict_logvar is None:
+        assert log_var is None
+
+
+@pytest.mark.parametrize("predict_logvar", ["pixelwise", None])
+@pytest.mark.parametrize("output_channels", [2, 3])
+def test_lvae_predict_mmse(minimum_lvae_params, predict_logvar, output_channels):
+    """Test MMSE prediction."""
+    minimum_lvae_params["predict_logvar"] = predict_logvar
+    minimum_lvae_params["output_channels"] = output_channels
+
+    input_shape = minimum_lvae_params["input_shape"]
+
+    # initialize model
+    model = LadderVAE(**minimum_lvae_params)
+    # initialize likelihoods (commented out of __init__ atm?)
+    model.create_likelihood_module()
+
+    # dummy input
+    x = torch.rand(size=(1, 1, input_shape, input_shape))
+    # prediction
+    y, log_var = lvae_predict_mmse(model, x, mmse_count=5)
+
+    assert y.shape == (1, output_channels, input_shape, input_shape)
+    if predict_logvar == "pixelwise":
+        assert log_var.shape == (1, output_channels, input_shape, input_shape)
+    elif predict_logvar is None:
+        assert log_var is None
