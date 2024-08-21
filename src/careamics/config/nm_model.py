580d8de3d8d5c0127f4c143fa7e4a5dab4f17e3f
federico-carrara <federico1.carrara@mail.polimi.it>
2024-08-21 14:32:11 +0200

    refac: fixed typing in GaussianMixtureNMConfig

--- src/careamics/config/nm_model.py
+++ src/careamics/config/nm_model.py
@@ -1,7 +1,8 @@
 """Noise models config."""
 
+import torch
 from pathlib import Path
-from typing import Any, Literal, Union
+from typing import Literal, Union
 
 from pydantic import BaseModel, ConfigDict, Field
 
@@ -24,7 +25,7 @@ class GaussianMixtureNMConfig(BaseModel):
     """Path to the directory where the trained noise model (*.npz) is saved in the
     `train` method."""
 
-    weight: Any = None
+    weight: torch.Tensor = None
     """A [3*n_gaussian, n_coeff] sized array containing the values of the weights
     describing the GMM noise model, with each row corresponding to one
     parameter of each gaussian, namely [mean, standard deviation and weight].
@@ -49,7 +50,7 @@ class GaussianMixtureNMConfig(BaseModel):
     max_signal: float = Field(default=1.0, ge=0.0)
     """Maximum signal intensity expected in the image."""
 
-    min_sigma: Any = None
+    min_sigma: float = None
     """All values of `standard deviation` below this are clamped to this value."""
 
     tol: float = Field(default=1e-10)
