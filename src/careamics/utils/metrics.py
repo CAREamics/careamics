c465fe695a1949a0b83ea1457ca120aac0909986
federico-carrara <federico1.carrara@mail.polimi.it>
2024-08-22 18:54:25 +0200

    refac: made metrics `torch` compatible + added docs + added `RunningPSNR`
    * updated tests to reflect changes

--- src/careamics/utils/metrics.py
+++ src/careamics/utils/metrics.py
@@ -10,8 +10,10 @@ import numpy as np
 import torch
 from skimage.metrics import peak_signal_noise_ratio
 
+Array = Union[np.ndarray, torch.Tensor]
 
-def psnr(gt: np.ndarray, pred: np.ndarray, range: float = 255.0) -> float:
+
+def psnr(gt: Array, pred: Array, range: float = 255.0) -> float:
     """
     Peak Signal to Noise Ratio.
 
@@ -20,9 +22,9 @@ def psnr(gt: np.ndarray, pred: np.ndarray, range: float = 255.0) -> float:
 
     Parameters
     ----------
-    gt : NumPy array
+    gt : Array
         Ground truth image.
-    pred : NumPy array
+    pred : Array
         Predicted image.
     range : float, optional
         The images pixel range, by default 255.0.
@@ -35,74 +37,72 @@ def psnr(gt: np.ndarray, pred: np.ndarray, range: float = 255.0) -> float:
     return peak_signal_noise_ratio(gt, pred, data_range=range)
 
 
-def _zero_mean(x: np.ndarray) -> np.ndarray:
+def _zero_mean(x: Array) -> Array:
     """
     Zero the mean of an array.
 
     Parameters
     ----------
-    x : NumPy array
+    x : Array
         Input array.
 
     Returns
     -------
-    NumPy array
+    Array
         Zero-mean array.
     """
-    return x - np.mean(x)
+    return x - x.mean()
 
 
-def _fix_range(gt: np.ndarray, x: np.ndarray) -> np.ndarray:
+def _fix_range(gt: Array, x: Array) -> Array:
     """
     Adjust the range of an array based on a reference ground-truth array.
 
     Parameters
     ----------
-    gt : np.ndarray
+    gt : Array
         Ground truth image.
-    x : np.ndarray
+    x : Array
         Input array.
 
     Returns
     -------
-    np.ndarray
+    Array
         Range-adjusted array.
     """
-    a = np.sum(gt * x) / (np.sum(x * x))
+    a = (gt * x).sum() / (x * x).sum()
     return x * a
 
 
-def _fix(gt: np.ndarray, x: np.ndarray) -> np.ndarray:
+def _fix(gt: Array, x: Array) -> Array:
     """
     Zero mean a groud truth array and adjust the range of the array.
 
     Parameters
     ----------
-    gt : np.ndarray
+    gt : Array
         Ground truth image.
-    x : np.ndarray
+    x : Array
         Input array.
 
     Returns
     -------
-    np.ndarray
+    Array
         Zero-mean and range-adjusted array.
     """
     gt_ = _zero_mean(gt)
     return _fix_range(gt_, _zero_mean(x))
 
 
-def scale_invariant_psnr(
-    gt: np.ndarray, pred: np.ndarray
-) -> Union[float, torch.tensor]:
+def scale_invariant_psnr(gt: Array, pred: Array) -> Union[float, torch.tensor]:
     """
     Scale invariant PSNR.
 
     Parameters
     ----------
-    gt : np.ndarray
+    gt : Array
         Ground truth image.
-    pred : np.ndarray
+    pred : Array
         Predicted image.
 
     Returns
@@ -110,6 +110,72 @@ def scale_invariant_psnr(
     Union[float, torch.tensor]
         Scale invariant PSNR value.
     """
-    range_parameter = (np.max(gt) - np.min(gt)) / np.std(gt)
-    gt_ = _zero_mean(gt) / np.std(gt)
+    range_parameter = (gt.max() - gt.min()) / gt.std()
+    gt_ = _zero_mean(gt) / gt.std()
     return psnr(_zero_mean(gt_), _fix(gt_, pred), range_parameter)
+
+
+class RunningPSNR:
+    """Compute the running PSNR during validation step in training.
+
+    This class allows to compute the PSNR on the entire validation set
+    one batch at the time.
+
+    Attributes
+    ----------
+    N : int
+        Number of elements seen so far during the epoch.
+    mse_sum : float
+        Running sum of the MSE over the N elements seen so far.
+    max : float
+        Running max value of the N target images seen so far.
+    min : float
+        Running min value of the N target images seen so far.
+    """
+
+    def __init__(self):
+        self.N = None
+        self.mse_sum = None
+        self.max = self.min = None
+        self.reset()
+
+    def reset(self):
+        """Reset the running PSNR computation.
+
+        Usually called at the end of each epoch.
+        """
+        self.mse_sum = 0
+        self.N = 0
+        self.max = self.min = None
+
+    def update(self, rec: torch.Tensor, tar: torch.Tensor) -> None:
+        """Update the running PSNR statistics given a new batch.
+
+        Parameters
+        ----------
+        rec: torch.Tensor
+            Batch of reconstructed images (B, C, H, W).
+        tar: torch.Tensor
+            Batch of target images (B, C, H, W).
+        """
+        ins_max = torch.max(tar).item()
+        ins_min = torch.min(tar).item()
+        if self.max is None:
+            assert self.min is None
+            self.max = ins_max
+            self.min = ins_min
+        else:
+            self.max = max(self.max, ins_max)
+            self.min = min(self.min, ins_min)
+
+        mse = (rec - tar) ** 2
+        elementwise_mse = torch.mean(mse.view(len(mse), -1), dim=1)
+        self.mse_sum += torch.nansum(elementwise_mse)
+        self.N += len(elementwise_mse) - torch.sum(torch.isnan(elementwise_mse))
+
+    def get(self):
+        """Get the actual PSNR value given the running statistics."""
+        if self.N == 0 or self.N is None:
+            return None
+        rmse = torch.sqrt(self.mse_sum / self.N)
+        return 20 * torch.log10((self.max - self.min) / rmse)
--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import torch
 
 from careamics.utils.metrics import (
     _zero_mean,
@@ -10,9 +11,10 @@ from careamics.utils.metrics import (
 @pytest.mark.parametrize(
     "x",
     [
-        5.6,
         np.array([1, 2, 3, 4, 5]),
         np.array([[1, 2, 3], [4, 5, 6]]),
+        torch.tensor([1, 2, 3, 4, 5]),
+        torch.tensor([[1, 2, 3], [4, 5, 6]]),
     ],
 )
 def test_zero_mean(x):
@@ -24,7 +26,16 @@ def test_zero_mean(x):
     [
         (np.array([1, 2, 3, 4, 5, 6]), np.array([1, 2, 3, 4, 5, 6]), 332.22),
         (np.array([[1, 2, 3], [4, 5, 6]]), np.array([[1, 2, 3], [4, 5, 6]]), 332.22),
+        (torch.tensor([1, 2, 3, 4, 5, 6]), torch.tensor([1, 2, 3, 4, 5, 6]), 332.22),
+        (
+            torch.tensor([[1, 2, 3], [4, 5, 6]]),
+            torch.tensor([[1, 2, 3], [4, 5, 6]]),
+            332.22,
+        ),
     ],
 )
 def test_scale_invariant_psnr(gt, pred, result):
     assert scale_invariant_psnr(gt, pred) == pytest.approx(result, rel=5e-3)
+
+
+# TODO: add tests for RunningPSNR
