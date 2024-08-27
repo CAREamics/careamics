0154e0c0403b671c837566a58cd788b0a900c131
federico-carrara <federico1.carrara@mail.polimi.it>
2024-08-27 18:04:59 +0200

    tests: updated existing tests

--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -10,15 +10,15 @@ from careamics.utils.metrics import (
 @pytest.mark.parametrize(
     "x",
     [
-        5.6,
         np.array([1, 2, 3, 4, 5]),
         np.array([[1, 2, 3], [4, 5, 6]]),
     ],
 )
-def test_zero_mean(x):
+def test_zero_mean(x: np.ndarray):
     assert np.allclose(_zero_mean(x), x - np.mean(x))
 
 
+# TODO: with 2 identical arrays, shouldn't the result be `inf`?
 @pytest.mark.parametrize(
     "gt, pred, result",
     [
@@ -26,5 +26,7 @@ def test_zero_mean(x):
         (np.array([[1, 2, 3], [4, 5, 6]]), np.array([[1, 2, 3], [4, 5, 6]]), 332.22),
     ],
 )
-def test_scale_invariant_psnr(gt, pred, result):
+def test_scale_invariant_psnr(gt: np.ndarray, pred: np.ndarray, result: float):
     assert scale_invariant_psnr(gt, pred) == pytest.approx(result, rel=5e-3)
+
+
