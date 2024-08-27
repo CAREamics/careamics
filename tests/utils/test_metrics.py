7448b505a8e798fece9f41fa60bd7a5a4e4ac326
federico-carrara <federico1.carrara@mail.polimi.it>
2024-08-27 21:28:52 +0200

    tests: added comments and TODOs

--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -7,6 +7,11 @@ from careamics.utils.metrics import (
     psnr
 )
 
+# TODO: add missing test cases
+# - `psnr` result
+# - `_fix` and `fix_range`
+# - `RunningPSNR`
+
 
 @pytest.mark.parametrize(
     "x",
@@ -40,7 +45,7 @@ def test_scale_invariant_psnr(gt: np.ndarray, pred: np.ndarray, result: float):
         np.int64
     ],
 )
-def test_psnr_working(data_type: np.dtype):
+def test_psnr(data_type: np.dtype):
     gt_ = np.random.randint(0, 255, size=(16, 16)).astype(data_type)
     pred_ = np.random.randint(0, 255, size=(16, 16)).astype(data_type)
     if data_type != np.float32:
@@ -48,3 +53,4 @@ def test_psnr_working(data_type: np.dtype):
     assert psnr(gt_, pred_, 255) is not None
     range_ = gt_.max() - gt_.min()
     assert psnr(gt_, pred_, range_) is not None
+    # add check on the result
