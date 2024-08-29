4bf99f138d7de92cdbcb624422d3e23c42551871
federico-carrara <federico1.carrara@mail.polimi.it>
2024-08-29 16:19:16 +0200

    refac: changed serialization of array fields; excluded large arrays + added custom serialization for small arrays

--- src/careamics/config/likelihood_model.py
+++ src/careamics/config/likelihood_model.py
@@ -4,7 +4,7 @@ from typing import Literal, Optional, Union
 
 import numpy as np
 import torch
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 from careamics.models.lvae.noise_models import (
     GaussianMixtureNoiseModel,
@@ -32,13 +32,15 @@ class NMLikelihoodConfig(BaseModel):
 
     model_config = ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
 
-    data_mean: Union[np.ndarray, torch.Tensor] = torch.zeros(1)
+    # TODO remove and use as parameters to the likelihood functions? 
+    data_mean: Union[np.ndarray, torch.Tensor] = Field(default=torch.zeros(1), exclude=True)
     """The mean of the data, used to unnormalize data for noise model evaluation.
     Shape is (target_ch,) (or (1, target_ch, [1], 1, 1))."""
 
-    data_std: Union[np.ndarray, torch.Tensor] = torch.ones(1)
+    # TODO remove and use as parameters to the likelihood functions? 
+    data_std: Union[np.ndarray, torch.Tensor] = Field(default=torch.ones(1), exclude=True)
     """The standard deviation of the data, used to unnormalize data for noise
     model evaluation. Shape is (target_ch,) (or (1, target_ch, [1], 1, 1))."""
 
-    noise_model: Union[NoiseModel, None] = None
+    noise_model: Optional[NoiseModel] = Field(default=None, exclude=True)
     """The noise model instance used to compute the likelihood."""
