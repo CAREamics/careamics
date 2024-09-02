85cce9dde63f2495fa8e7afa10af578888812a74
federico-carrara <federico1.carrara@mail.polimi.it>
2024-09-02 16:10:31 +0200

    fix: not allowing `data_range == None` anymore in `psnr()` to avoid unwanted behavior from skimage function

--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -4,6 +4,7 @@ import pytest
 from careamics.utils.metrics import (
     _zero_mean,
     scale_invariant_psnr,
+    psnr
 )
 
 
@@ -28,3 +29,10 @@ def test_zero_mean(x):
 )
 def test_scale_invariant_psnr(gt, pred, result):
     assert scale_invariant_psnr(gt, pred) == pytest.approx(result, rel=5e-3)
+
+
+def test_psnr_no_range():
+    gt_ = np.random.rand(8, 8)
+    pred_ = np.random.rand(8, 8)
+    with pytest.raises(ValueError):
+        psnr(gt_, pred_, None)
