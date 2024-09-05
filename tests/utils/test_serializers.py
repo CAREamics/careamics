0b8cbf7f7e4e2d9927f861fb31d325c7f558c97a
federico-carrara <federico1.carrara@mail.polimi.it>
2024-09-05 17:54:04 +0200

    tests: updated deserializ tests

--- tests/utils/test_serializers.py
+++ tests/utils/test_serializers.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from typing import Union
 
 import numpy as np
@@ -37,15 +39,27 @@ def test_serialize_tensor(arr: Union[np.ndarray, torch.Tensor]):
     assert arr_model.model_dump() == {"arr": "[1, 2]"}
 
 
-def test_deserialize_array():
+def test_deserialize_array(tmp: Path):
     """Test list_to_numpy function."""
-    config = {"arr": [1, 2]}
-    arr_model = MyArray(**config)
-    assert np.array_equal(arr_model.arr, np.array([1, 2]))
+    arr_model = MyArray(arr=np.array([1, 2]))
+    # save to JSON
+    with open(tmp / "array_config.json", "w") as f:
+        f.write(arr_model.model_dump_json())
+    # load from JSON
+    with open(tmp / "array_config.json") as f:
+        config = json.load(f)
+    new_arr_model = MyArray(**config)
+    assert np.array_equal(new_arr_model.arr, np.array([1, 2]))
 
 
-def test_deserialize_tensor():
+def test_deserialize_tensor(tmp: Path):
     """Test list_to_tensor function."""
-    config = {"arr": [1, 2]}
-    arr_model = MyTensor(**config)
-    assert torch.equal(arr_model.arr, torch.tensor([1, 2]))
+    arr_model = MyTensor(arr=torch.tensor([1, 2]))
+    # save to JSON
+    with open(tmp / "tensor_config.json", "w") as f:
+        f.write(arr_model.model_dump_json())
+    # load from JSON
+    with open(tmp / "tensor_config.json") as f:
+        config = json.load(f)
+    new_arr_model = MyTensor(**config)
+    assert torch.equal(new_arr_model.arr, torch.tensor([1, 2]))
