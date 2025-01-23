0d8b349c430fc94f437d7448745c92c04d94a9a5
melisande-c <milly.croft@gmail.com>
2025-01-23 10:39:13 +0100

    refac: mv array readers to subdirectory; rename protocol file and class

--- src/careamics/dataset_dev/data_reader/array_reader/array_reader_protocol.py
+++ src/careamics/dataset_dev/data_reader/array_reader/array_reader_protocol.py
@@ -4,7 +4,7 @@ from typing import Literal, Protocol, Union
 from numpy.typing import NDArray
 
 
-class ProtoArrayReader(Protocol):
+class ArrayReaderProtocol(Protocol):
 
     # TODO: not sure how compatible using Path will be for a zarr array
     #   (for a zarr array need to specify file path and internal zarr path)
