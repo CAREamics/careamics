6a00b5cedd9d4f28c98182ee5909572addf9110a
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2025-02-10 18:23:02 +0100

    fix: Guard against CI CAREamics version in editable mode

--- src/careamics/utils/version.py
+++ src/careamics/utils/version.py
@@ -15,4 +15,10 @@ def get_clean_version() -> str:
         Clean CAREamics version.
     """
     parts = __version__.split(".")  # Remove any local version identifier
+
+    # for local installs that do not detect the latest versions via tags
+    # (typically our CI)
+    if "dev" in parts[-1]:
+        parts[-1] = "*"
+
     return ".".join(parts[:3])
