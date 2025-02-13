e0ede24f6fb19e2b1b793d6738c11915c0c18baa
CatEek <zubarev.ia@gmail.com>
2025-02-13 16:07:16 +0100

    ds tests fix

--- tests/dataset/test_in_memory_pred_dataset.py
+++ tests/dataset/test_in_memory_pred_dataset.py
@@ -57,7 +57,7 @@ def test_correct_normalized_outputs(shape, axes, expected_shape):
 
     # check that the dataset returns normalized images
     for i in range(len(dataset)):
-        img = dataset[i][0]
+        img = dataset[i]
 
         # check that it has the correct shape
         assert img.shape == expected_shape
