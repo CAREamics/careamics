3fd3793ce50ead346a9fd7636f6ebd120c6ed6d9
veegalinova <veegalinova@gmail.com>
2025-02-25 11:32:36 +0100

    swap init and generate parameters

--- src/careamics/dataset_ng/patching_strategies/patch_specs_generator.py
+++ src/careamics/dataset_ng/patching_strategies/patch_specs_generator.py
@@ -13,43 +13,43 @@ P = ParamSpec("P")
 class PatchSpecsGenerator(Protocol[P]):
 
     def generate(
-        self, patch_size: Sequence[int], *args: P.args, **kwargs: P.kwargs
+        self, data_shapes: Sequence[Sequence[int]], *args: P.args, **kwargs: P.kwargs
     ) -> list[PatchSpecs]: ...
 
     # Should return the number of patches that will be produced for a set of args
     # Will be for mapped dataset length
     def n_patches(
-        self, patch_size: Sequence[int], *args: P.args, **kwargs: P.kwargs
+        self, data_shapes: Sequence[Sequence[int]], *args: P.args, **kwargs: P.kwargs
     ) -> int: ...
 
 
 class RandomPatchSpecsGenerator:
 
-    def __init__(self, data_shapes: Sequence[Sequence[int]], random_seed: int):
-        self.data_shapes = data_shapes
+    def __init__(self, patch_size: Sequence[int], random_seed: int):
+        self.patch_size = patch_size
         self.random_seed = random_seed
 
-    def generate(self, patch_size: Sequence[int]) -> list[PatchSpecs]:
+    def generate(self, data_shapes: Sequence[Sequence[int]]) -> list[PatchSpecs]:
         rng = np.random.default_rng(seed=self.random_seed)
         patch_specs: list[PatchSpecs] = []
-        for data_idx, data_shape in enumerate(self.data_shapes):
+        for data_idx, data_shape in enumerate(data_shapes):
 
             # shape on which data is patched
-            data_spatial_shape = data_shape[-len(patch_size) :]
+            data_spatial_shape = data_shape[-len(self.patch_size) :]
 
-            n_patches = self._n_patches_in_sample(patch_size, data_spatial_shape)
+            n_patches = self._n_patches_in_sample(self.patch_size, data_spatial_shape)
             data_patch_specs = [
                 PatchSpecs(
                     data_idx=data_idx,
                     sample_idx=sample_idx,
                     coords=tuple(
                         rng.integers(
-                            np.zeros(len(patch_size), dtype=int),
-                            np.array(data_spatial_shape) - np.array(patch_size),
+                            np.zeros(len(self.patch_size), dtype=int),
+                            np.array(data_spatial_shape) - np.array(self.patch_size),
                             endpoint=True,
                         )
                     ),
-                    patch_size=patch_size,
+                    patch_size=self.patch_size,
                 )
                 for sample_idx in range(data_shape[0])
                 for _ in range(n_patches)
@@ -59,17 +59,17 @@ class RandomPatchSpecsGenerator:
 
     # NOTE: enerate and n_patches methods must have matching signatures
     #   as dictated by protocol
-    def n_patches(self, patch_size: Sequence[int]) -> int:
+    def n_patches(self, data_shapes: Sequence[Sequence[int]]) -> int:
         n_sample_patches: NDArray[np.int_] = np.array(
             [
-                self._n_patches_in_sample(patch_size, data_shape[-len(patch_size) :])
-                for data_shape in self.data_shapes
+                self._n_patches_in_sample(
+                    self.patch_size, data_shape[-len(self.patch_size) :]
+                )
+                for data_shape in data_shapes
             ],
             dtype=int,
         )
-        n_samples = np.array(
-            [data_shape[0] for data_shape in self.data_shapes], dtype=int
-        )
+        n_samples = np.array([data_shape[0] for data_shape in data_shapes], dtype=int)
         n_data_patches = n_samples * n_sample_patches
         return int(n_data_patches.sum())
 
@@ -88,12 +88,10 @@ class RandomPatchSpecsGenerator:
 # TODO: this is an unfinished prototype based on current tiling implementation
 #  not guaranteed to work!
 class SequentialPatchSpecsGenerator:
-    def __init__(
-        self, data_shapes: Sequence[Sequence[int]], overlap: Optional[list[int]] = None
-    ):
-        self.data_shapes = data_shapes
+    def __init__(self, patch_size: Sequence[int], overlap: Optional[list[int]] = None):
+        self.patch_size = patch_size
         if overlap is None:
-            overlap = [0] * len(data_shapes[0])
+            overlap = [0] * len(patch_size)
         self.overlap = np.asarray(overlap)
 
     def _compute_coords_1d(
@@ -127,16 +125,16 @@ class SequentialPatchSpecsGenerator:
         )
         return int(np.prod(steps))
 
-    def generate(self, patch_size: Sequence[int]) -> list[PatchSpecs]:
+    def generate(self, data_shapes: Sequence[Sequence[int]]) -> list[PatchSpecs]:
         patch_specs: list[PatchSpecs] = []
-        for data_idx, data_shape in enumerate(self.data_shapes):
+        for data_idx, data_shape in enumerate(data_shapes):
 
-            data_spatial_shape = data_shape[-len(patch_size) :]
+            data_spatial_shape = data_shape[-len(self.patch_size) :]
             coords_list = [
                 self._compute_coords_1d(
-                    patch_size[i], data_spatial_shape[i], self.overlap[i]
+                    self.patch_size[i], data_spatial_shape[i], self.overlap[i]
                 )
-                for i in range(len(patch_size))
+                for i in range(len(self.patch_size))
             ]
             for sample_idx in range(data_shape[0]):
                 for crop_coord in itertools.product(*coords_list):
@@ -145,22 +143,22 @@ class SequentialPatchSpecsGenerator:
                             data_idx=data_idx,
                             sample_idx=sample_idx,
                             coords=tuple([coord[0] for coord in crop_coord]),
-                            patch_size=patch_size,
+                            patch_size=self.patch_size,
                         )
                     )
         return patch_specs
 
-    def n_patches(self, patch_size: Sequence[int]) -> int:
+    def n_patches(self, data_shapes: Sequence[Sequence[int]]) -> int:
         n_sample_patches: NDArray[np.int_] = np.array(
             [
-                self._n_patches_in_sample(patch_size, data_shape[-len(patch_size) :])
-                for data_shape in self.data_shapes
+                self._n_patches_in_sample(
+                    self.patch_size, data_shape[-len(self.patch_size) :]
+                )
+                for data_shape in data_shapes
             ],
             dtype=int,
         )
-        n_samples = np.array(
-            [data_shape[0] for data_shape in self.data_shapes], dtype=int
-        )
+        n_samples = np.array([data_shape[0] for data_shape in data_shapes], dtype=int)
         n_data_patches = n_samples * n_sample_patches
         return int(n_data_patches.sum())
 
@@ -168,5 +166,6 @@ class SequentialPatchSpecsGenerator:
 if __name__ == "__main__":
     # testing mypy accepts protocol type
     patch_specs_generator: PatchSpecsGenerator = RandomPatchSpecsGenerator(
-        [(1, 1, 6, 6), (1, 1, 4, 4)], random_seed=42
+        patch_size=(2, 2), random_seed=42
     )
+    patch_specs_generator.generate([(1, 1, 6, 6), (1, 1, 4, 4)])
