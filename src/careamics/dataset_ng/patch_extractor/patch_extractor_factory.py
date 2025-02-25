4d146519928b1798212bb41c09193318ed85a3df
veegalinova <veegalinova@gmail.com>
2025-02-25 11:54:56 +0100

    change create_patch_extractor to return only one extractor, add types

--- src/careamics/dataset_ng/patch_extractor/patch_extractor_factory.py
+++ src/careamics/dataset_ng/patch_extractor/patch_extractor_factory.py
@@ -1,10 +1,11 @@
 from collections.abc import Sequence
 from pathlib import Path
-from typing import Optional, Union
+from typing import Union
 
 from numpy.typing import NDArray
 
 from careamics.config import DataConfig
+from careamics.config.inference_model import InferenceConfig
 from careamics.config.support import SupportedData
 from careamics.dataset_ng.patch_extractor import (
     PatchExtractor,
@@ -13,7 +14,7 @@ from careamics.dataset_ng.patch_extractor import (
 
 
 def get_patch_extractor_constructor(
-    data_config: DataConfig,
+    data_config: Union[DataConfig, InferenceConfig],
 ) -> PatchExtractorConstructor:
     if data_config.data_type == SupportedData.ARRAY:
         return PatchExtractor.from_arrays
@@ -25,12 +26,11 @@ def get_patch_extractor_constructor(
         raise ValueError(f"Data type {data_config.data_type} is not supported.")
 
 
-def create_patch_extractors(
-    data_config: DataConfig,
+def create_patch_extractor(
+    data_config: Union[DataConfig, InferenceConfig],
     data: Union[Sequence[NDArray], Sequence[Path]],
-    target_data: Optional[Union[Sequence[NDArray], Sequence[Path]]] = None,
     **kwargs,
-) -> tuple[PatchExtractor, Optional[PatchExtractor]]:
+) -> PatchExtractor:
     # get correct constructor
     constructor = get_patch_extractor_constructor(data_config)
 
@@ -39,12 +39,5 @@ def create_patch_extractors(
 
     # --- data extractor
     patch_extractor: PatchExtractor = constructor(source=data, **constructor_kwargs)
-    # --- optional target extractor
-    if target_data is not None:
-        target_patch_extractor: PatchExtractor = constructor(
-            source=target_data, **constructor_kwargs
-        )
 
-        return patch_extractor, target_patch_extractor
-
-    return patch_extractor, None
+    return patch_extractor
