4c29426758d4a0aa770117556736e558ef84126d
melisande-c <milly.croft@gmail.com>
2025-03-14 19:04:00 +0100

    docs: add docstrings and comments

--- src/careamics/dataset_ng/patching_strategies/patching_strategy_protocol.py
+++ src/careamics/dataset_ng/patching_strategies/patching_strategy_protocol.py
@@ -29,6 +29,32 @@ class PatchSpecs(TypedDict):
 
 
 class TileSpecs(PatchSpecs):
+    """A dictionary that specifies a single patch in a series of `ImageStacks`.
+
+    Attributes
+    ----------
+    data_idx: int
+        Determines which `ImageStack` a patch belongs to, within a series of
+        `ImageStack`s.
+    sample_idx: int
+        Determines which sample a patch belongs to, within an `ImageStack`.
+    coords: sequence of int
+        The top-left (and first z-slice for 3D data) of a patch. The sequence will have
+        length 2 or 3, for 2D and 3D data respectively.
+    patch_size: sequence of int
+        The size of the patch. The sequence will have length 2 or 3, for 2D and 3D data
+        respectively.
+    data_shape: sequence of int
+        The original shape of the data that the tile came from.
+    crop_coords: sequence of int
+        The top-left side of where the tile will be cropped, in coordinates relative
+        to the tile.
+    crop_size: sequence of int
+        The size of the cropped tile.
+    stitch_coords: sequence of int
+        Where the tile will be stitched back into an image, taking into account
+        that the tile will be cropped, in coords relative to the image.
+    """
 
     # temporarily adding data_shape, it will be included in the ImageRegion (PR #420)
     #   so it can be recovered from there instead when #420 is merged
