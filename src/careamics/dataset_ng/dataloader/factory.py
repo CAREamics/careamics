4dbb25550586058c59bc9df4d6d4d61ff914f044
melisande-c <milly.croft@gmail.com>
2025-03-27 16:05:00 +0100

    refac: dataset and dataloader factories - extract case switching to determine_dataset_type function

--- src/careamics/dataset_ng/dataloader/factory.py
+++ src/careamics/dataset_ng/dataloader/factory.py
@@ -1,7 +1,7 @@
 import warnings
 from collections.abc import Sequence
 from pathlib import Path
-from typing import Any, Literal, Optional, TypeVar, Union
+from typing import Any, Optional, TypeVar, Union
 
 import numpy as np
 from torch.utils.data import DataLoader
@@ -13,13 +13,14 @@ from careamics.file_io.read import ReadFunc
 from careamics.utils import get_ram_size
 
 from ..batch_sampler import FifoBatchSampler
-from ..dataset import Mode
+from ..dataset import CareamicsDataset, Mode
 from ..dataset.factory import (
+    DatasetType,
     create_array_dataset,
     create_custom_file_dataset,
     create_custom_image_stack_dataset,
     create_lazy_tiff_dataset,
-    create_loaded_tiff_dataset,
+    determine_dataset_type,
 )
 from ..patch_extractor import ImageStackLoader
 from ..patch_extractor.image_stack import (
@@ -51,61 +52,71 @@ def create_dataloader(
     image_stack_loader: Optional[ImageStackLoader] = None,
     image_stack_loader_kwargs: Optional[dict[str, Any]] = None,
     **dataloader_kwargs: Any,
-):
-    if config.data_type == SupportedData.ARRAY:
+) -> DataLoader[CareamicsDataset[ImageStack]]:
+    # calculate total size
+    all_files = list(inputs)
+    if targets is not None:
+        all_files.extend(targets)
+    total_file_size = get_files_size(all_files)
+    in_memory: bool = total_file_size < get_ram_size() * max_mem_use
+
+    data_type = SupportedData(config.data_type)
+
+    dataset_type = determine_dataset_type(
+        data_type, in_memory, read_func, image_stack_loader
+    )
+    if dataset_type == DatasetType.ARRAY:
         dataset = create_array_dataset(config, mode, inputs, targets)
         return DataLoader(dataset, batch_size, **dataloader_kwargs)
-    elif config.data_type == SupportedData.TIFF:
-        dataloader = create_tiff_dataloader(
+    elif dataset_type == DatasetType.IN_MEM_TIFF:
+        dataset = create_lazy_tiff_dataset(config, mode, inputs, targets)
+        return DataLoader(dataset, batch_size, **dataloader_kwargs)
+    elif dataset_type == DatasetType.LAZY_TIFF:
+        max_files = calc_max_files_loadable(all_files, max_mem_use)
+        # make max files even because fifo_manager is shared between input and target
+        if (max_files % 2 != 0) and (max_files != 1):
+            max_files = max_files - 1
+        return create_lazy_tiff_dataloader(
             config,
             mode,
             inputs,
             targets,
             batch_size,
-            max_mem_use=max_mem_use,
+            max_files_loaded=max_files,
             **dataloader_kwargs,
         )
-        return dataloader
-    elif config.data_type == SupportedData.CUSTOM:
-        if read_func is not None:
-            if read_kwargs is None:
-                read_kwargs = {}
-            return create_custom_file_dataset(
-                config,
-                mode,
-                inputs,
-                targets,
-                read_func=read_func,
-                read_kwargs=read_kwargs,
-            )
-        elif image_stack_loader is not None:
-            if image_stack_loader_kwargs is None:
-                image_stack_loader_kwargs = {}
-            return create_custom_image_stack_dataset(
-                config,
-                mode,
-                inputs,
-                targets,
-                image_stack_loader=image_stack_loader,
-                **image_stack_loader_kwargs,
-            )
-        else:
-            raise ValueError(
-                "Found `data_type='custom'` but no `read_func` or `image_stack_loader` "
-                "has been provided."
-            )
-    # TODO: Zarr
+    elif data_type == DatasetType.IN_MEM_CUSTOM_FILE:
+        if read_kwargs is None:
+            read_kwargs = {}
+        assert read_func is not None  # should be true from `determine_dataset_type`
+        dataset = create_custom_file_dataset(
+            config, mode, inputs, targets, read_func=read_func, read_kwargs=read_kwargs
+        )
+        return DataLoader(dataset, batch_size, **dataloader_kwargs)
+    elif data_type == DatasetType.CUSTOM_IMAGE_STACK:
+        if image_stack_loader_kwargs is None:
+            image_stack_loader_kwargs = {}
+        assert image_stack_loader is not None  # should be true
+        dataset = create_custom_image_stack_dataset(
+            config,
+            mode,
+            inputs,
+            targets,
+            image_stack_loader=image_stack_loader,
+            **image_stack_loader_kwargs,
+        )
+        return DataLoader(dataset, batch_size, **dataloader_kwargs)
     else:
-        raise ValueError(f"Unrecognised `data_type`, '{config.data_type}'.")
+        raise ValueError(f"Unrecognized dataset type, {dataset_type}.")
 
 
-def create_tiff_dataloader(
+def create_lazy_tiff_dataloader(
     config: Union[DataConfig, InferenceConfig],
     mode: Mode,
     inputs: Sequence[Path],
     targets: Optional[Sequence[Path]] = None,
     batch_size: int = 1,
-    max_mem_use: float = 0.8,
+    max_files_loaded: int = 2,
     **dataloader_kwargs: Any,
 ) -> DataLoader:
     data_type = SupportedData(config.data_type)
@@ -115,40 +126,33 @@ def create_tiff_dataloader(
             f"'tiff' found {DataConfig.data_type}."
         )
 
-    # calculate total size
-    all_files = list(inputs)
-    if targets is not None:
-        all_files.extend(targets)
-    total_file_size = get_files_size(all_files)
-    in_memory: Literal[True, False] = total_file_size < get_ram_size() * max_mem_use
-
-    if in_memory:
-        dataset = create_loaded_tiff_dataset(config, mode, inputs, targets)
-        data_loader = DataLoader(dataset=dataset, **dataloader_kwargs)
-    else:
-        num_workers = dataloader_kwargs.get("num_workers", 0)
-        if num_workers > 0:
-            warnings.warn(
-                "Using lazy loading and found `num_workers>0`, but lazy loading has "
-                "not been optimized for multiprocessing. We suggest setting "
-                "`num_workers=0`. Note: Lazy loading has been selected because the "
-                "data has been calculated to not fit into memory.",
-                category=Warning,
-                stacklevel=1,
-            )
-        dataset = create_lazy_tiff_dataset(config, mode, inputs, targets)
-        batch_sampler_kwargs = {}
-        if "shuffle" in dataloader_kwargs:
-            batch_sampler_kwargs["shuffle"] = dataloader_kwargs["shuffle"]
-            del dataloader_kwargs["shuffle"]
-        if "drop_last" in dataloader_kwargs:
-            batch_sampler_kwargs["drop_last"] = dataloader_kwargs["drop_last"]
-            del dataloader_kwargs["drop_last"]
-
-        batch_sampler = FifoBatchSampler(
-            dataset=dataset, batch_size=batch_size, **batch_sampler_kwargs
+    num_workers = dataloader_kwargs.get("num_workers", 0)
+    if num_workers > 0:
+        warnings.warn(
+            "Using lazy loading and found `num_workers>0`, but lazy loading has "
+            "not been optimized for multiprocessing. We suggest setting "
+            "`num_workers=0`. Note: Lazy loading has been selected because the "
+            "data has been calculated to not fit into memory.",
+            category=Warning,
+            stacklevel=1,
         )
-        data_loader = DataLoader(
-            dataset=dataset, batch_sampler=batch_sampler, **dataloader_kwargs
-        )
+    dataset = create_lazy_tiff_dataset(config, mode, inputs, targets)
+    batch_sampler_kwargs = {}
+    if "shuffle" in dataloader_kwargs:
+        batch_sampler_kwargs["shuffle"] = dataloader_kwargs["shuffle"]
+        del dataloader_kwargs["shuffle"]
+    if "drop_last" in dataloader_kwargs:
+        batch_sampler_kwargs["drop_last"] = dataloader_kwargs["drop_last"]
+        del dataloader_kwargs["drop_last"]
+
+    batch_sampler = FifoBatchSampler(
+        dataset=dataset,
+        batch_size=batch_size,
+        max_files_loaded=max_files_loaded,
+        # TODO: set random seed arg?
+        **batch_sampler_kwargs,
+    )
+    data_loader = DataLoader(
+        dataset=dataset, batch_sampler=batch_sampler, **dataloader_kwargs
+    )
     return data_loader
--- src/careamics/dataset_ng/dataset/factory.py
+++ src/careamics/dataset_ng/dataset/factory.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
+from enum import Enum
 from pathlib import Path
 from typing import Any, Optional, TypeVar, Union
 
@@ -6,6 +7,7 @@ from numpy.typing import NDArray
 from typing_extensions import ParamSpec
 
 from careamics.config import DataConfig, InferenceConfig
+from careamics.config.support import SupportedData
 from careamics.file_io.read import ReadFunc
 
 from ..patch_extractor import ImageStackLoader, PatchExtractor
@@ -28,12 +30,117 @@ P = ParamSpec("P")
 GenericImageStack = TypeVar("GenericImageStack", bound=ImageStack, covariant=True)
 
 
+class DatasetType(Enum):
+    ARRAY = "array"
+    IN_MEM_TIFF = "in_mem_tiff"
+    LAZY_TIFF = "lazy_tiff"
+    IN_MEM_CUSTOM_FILE = "in_mem_custom_file"
+    OME_ZARR = "ome_zarr"
+    CUSTOM_IMAGE_STACK = "custom_image_stack"
+
+
+# bit of a mess of if-else statements
+def determine_dataset_type(
+    data_type: SupportedData,
+    in_memory: Optional[bool] = None,
+    read_func: Optional[ReadFunc] = None,
+    image_stack_loader: Optional[ImageStackLoader] = None,
+) -> DatasetType:
+    if data_type == SupportedData.ARRAY:
+        # TODO: ignoring in_memory arg, error if False?
+        return DatasetType.ARRAY
+    elif data_type == SupportedData.TIFF:
+        if in_memory:
+            return DatasetType.IN_MEM_TIFF
+        else:
+            return DatasetType.LAZY_TIFF
+    elif data_type == SupportedData.CUSTOM:
+        if read_func is not None:
+            if in_memory:
+                return DatasetType.IN_MEM_CUSTOM_FILE
+            else:
+                raise NotImplementedError(
+                    "Lazy loading has not been implemented for custom file types yet."
+                )
+        elif image_stack_loader is not None:
+            # TODO: ignoring im_memory arg
+            return DatasetType.CUSTOM_IMAGE_STACK
+        else:
+            raise ValueError(
+                "Found `data_type='custom'` but no `read_func` or `image_stack_loader` "
+                "has been provided."
+            )
+    # TODO: ZARR
+    else:
+        raise ValueError(f"Unrecognized `data_type`, '{data_type}'.")
+
+
+# convenience function but should use `create_dataloader function instead`
+# For lazy loading custom batch sampler also needs to be set.
+def create_dataset(
+    config: Union[DataConfig, InferenceConfig],
+    mode: Mode,
+    inputs: Any,
+    targets: Any,
+    in_memory: Optional[bool] = None,
+    read_func: Optional[ReadFunc] = None,
+    read_kwargs: Optional[dict[str, Any]] = None,
+    image_stack_loader: Optional[ImageStackLoader] = None,
+    image_stack_loader_kwargs: Optional[dict[str, Any]] = None,
+) -> CareamicsDataset[ImageStack]:
+    data_type = SupportedData(config.data_type)
+    dataset_type = determine_dataset_type(
+        data_type, in_memory, read_func, image_stack_loader
+    )
+    if dataset_type == DatasetType.ARRAY:
+        return create_array_dataset(config, mode, inputs, targets)
+    elif dataset_type == DatasetType.IN_MEM_TIFF:
+        return create_loaded_tiff_dataset(config, mode, inputs, targets)
+    elif dataset_type == DatasetType.LAZY_TIFF:
+        return create_lazy_tiff_dataset(config, mode, inputs, targets)
+    elif data_type == DatasetType.IN_MEM_CUSTOM_FILE:
+        if read_kwargs is None:
+            read_kwargs = {}
+        assert read_func is not None  # should be true from `determine_dataset_type`
+        return create_custom_file_dataset(
+            config, mode, inputs, targets, read_func=read_func, read_kwargs=read_kwargs
+        )
+    elif data_type == DatasetType.CUSTOM_IMAGE_STACK:
+        if image_stack_loader_kwargs is None:
+            image_stack_loader_kwargs = {}
+        assert image_stack_loader is not None  # should be true
+        return create_custom_image_stack_dataset(
+            config, mode, inputs, targets, image_stack_loader, image_stack_loader_kwargs
+        )
+    else:
+        raise ValueError(f"Unrecognized dataset type, {dataset_type}.")
+
+
 def create_array_dataset(
     config: Union[DataConfig, InferenceConfig],
     mode: Mode,
     inputs: Sequence[NDArray[Any]],
     targets: Optional[Sequence[NDArray[Any]]],
 ) -> CareamicsDataset[InMemoryImageStack]:
+    """
+    Create a CAREamicsDataset from array data.
+
+    Parameters
+    ----------
+    config : Union[DataConfig, InferenceConfig]
+        _description_
+    mode : Mode
+        _description_
+    inputs : Sequence[NDArray[Any]]
+        _description_
+    targets : Optional[Sequence[NDArray[Any]]]
+        _description_
+
+    Returns
+    -------
+    CareamicsDataset[InMemoryImageStack]
+        _description_
+    """
     input_extractor = create_array_extractor(source=inputs, axes=config.axes)
     target_extractor: Optional[PatchExtractor[InMemoryImageStack]]
     if targets is not None:
