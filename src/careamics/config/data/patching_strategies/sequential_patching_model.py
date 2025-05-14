cd5f2d7fd20d18321f718f0af67ab047fe0947f1
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2025-05-14 16:49:10 +0200

    Refactoring and tests

--- src/careamics/config/data/patching_strategies/sequential_patching_model.py
+++ src/careamics/config/data/patching_strategies/sequential_patching_model.py
@@ -1,81 +1,12 @@
 """Sequential patching Pydantic model."""
 
-from collections.abc import Sequence
-from typing import Literal, Optional
+from typing import Literal
 
-from pydantic import Field, ValidationInfo, field_validator
-
-from .patched_model import PatchedModel
+from .overlapping_patched_model import OverlappingPatchedModel
 
 
-class SequentialPatchingModel(PatchedModel):
+class SequentialPatchingModel(OverlappingPatchedModel):
     """Sequential patching Pydantic model."""
 
     name: Literal["sequential"] = "sequential"
     """The name of the patching strategy."""
-
-    overlap: Optional[Sequence[int]] = Field(
-        default=None,
-        min_length=2,
-        max_length=3,
-    )
-    """The overlap between patches in each spatial dimension, if None, no overlap is
-    applied. The overlap must be smaller than the patch size in each spatial dimension.
-    """
-
-    @field_validator("overlap")
-    @classmethod
-    def overlap_smaller_than_patch_size(
-        cls, overlap: Optional[Sequence[int]], values: ValidationInfo
-    ) -> Optional[Sequence[int]]:
-        """
-        Validate overlap.
-
-        Overlap must be smaller than the patch size in each spatial dimension.
-
-        Parameters
-        ----------
-        overlap : Sequence of int
-            Overlap.
-        values : ValidationInfo
-            Dictionary of values.
-
-        Returns
-        -------
-        Sequence of int
-            Validated overlap.
-        """
-        if overlap is None:
-            return None
-
-        patch_size = values.data["patch_size"]
-        if any(o >= p for o, p in zip(overlap, patch_size)):
-            raise ValueError("Overlap must be smaller than the patch size.")
-
-        return overlap
-
-    @field_validator("overlap")
-    @classmethod
-    def overlap_even(cls, overlap: Optional[Sequence[int]]) -> Optional[Sequence[int]]:
-        """
-        Validate overlap.
-
-        Overlap must be even.
-
-        Parameters
-        ----------
-        overlap : Sequence of int
-            Overlap.
-
-        Returns
-        -------
-        Sequence of int
-            Validated overlap.
-        """
-        if overlap is None:
-            return None
-
-        if any(o % 2 != 0 for o in overlap):
-            raise ValueError("Overlap must be even.")
-
-        return overlap
--- src/careamics/config/data/patching_strategies/tiled_patching_model.py
+++ src/careamics/config/data/patching_strategies/tiled_patching_model.py
@@ -1,74 +1,13 @@
 """Tiled patching Pydantic model."""
 
-from collections.abc import Sequence
 from typing import Literal
 
-from pydantic import Field, ValidationInfo, field_validator
-
-from .patched_model import PatchedModel
+from .overlapping_patched_model import OverlappingPatchedModel
 
 
-class TiledPatchingModel(PatchedModel):
+# TODO with UNet tiling must obey different rules than sequential tiling
+class TiledPatchingModel(OverlappingPatchedModel):
     """Tiled patching Pydantic model."""
 
     name: Literal["tiled"] = "tiled"
     """The name of the patching strategy."""
-
-    overlap: Sequence[int] = Field(
-        default=...,
-        min_length=2,
-        max_length=3,
-    )
-    """The overlap between patches in each spatial dimension. The overlap must be
-    smaller than the patch size in each spatial dimension."""
-
-    @field_validator("overlap")
-    @classmethod
-    def overlap_smaller_than_patch_size(
-        cls, overlap: Sequence[int], values: ValidationInfo
-    ) -> Sequence[int]:
-        """
-        Validate overlap.
-
-        Overlap must be smaller than the patch size in each spatial dimension.
-
-        Parameters
-        ----------
-        overlap : Sequence of int
-            Overlap.
-        values : ValidationInfo
-            Dictionary of values.
-
-        Returns
-        -------
-        Sequence of int
-            Validated overlap.
-        """
-        patch_size: Sequence[int] = values.data["patch_size"]
-        if any(o >= p for o, p in zip(overlap, patch_size)):
-            raise ValueError("Overlap must be smaller than the patch size.")
-
-        return overlap
-
-    @field_validator("overlap")
-    @classmethod
-    def overlap_even(cls, overlap: Sequence[int]) -> Sequence[int]:
-        """
-        Validate overlap.
-
-        Overlap must be even.
-
-        Parameters
-        ----------
-        overlap : Sequence of int
-            Overlap.
-
-        Returns
-        -------
-        Sequence of int
-            Validated overlap.
-        """
-        if any(o % 2 != 0 for o in overlap):
-            raise ValueError("Overlap must be even.")
-
-        return overlap
