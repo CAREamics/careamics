00d8b9c78a149ff7a55ba02d861b02b48a9bba83
melisande-c <milly.croft@gmail.com>
2025-07-02 11:28:27 +0200

    style: remove commented out code

--- src/careamics/models/unet.py
+++ src/careamics/models/unet.py
@@ -205,19 +205,19 @@ class UnetDecoder(nn.Module):
         decoder_blocks: list[nn.Module] = []
         for n in range(depth):
             decoder_blocks.append(upsampling)
-            # in_channels = (num_channels_init * 2 ** (depth - n)) * groups
-            # if (n > 0) and not (n2v2 and (n == depth - 1)):
-            #     in_channels = in_channels + in_channels // 2
-            in_channels = (num_channels_init * 2 ** (depth - n - 1)) * groups
-            out_channels = in_channels // 2 if n != depth - 1 else in_channels
 
+            in_channels = (num_channels_init * 2 ** (depth - n - 1)) * groups
+            # final decoder block has the same number in and out features
+            out_channels = in_channels // 2 if n != depth - 1 else in_channels
             if not (n2v2 and (n == depth - 1)):
                 in_channels = in_channels * 2  # accounting for skip connection concat
+
             decoder_blocks.append(
                 Conv_Block(
                     conv_dim,
                     in_channels=in_channels,
                     out_channels=out_channels,
+                    # final decoder block doesn't multiply the intermediate features
                     intermediate_channel_multiplier=2 if n != depth - 1 else 1,
                     dropout_perc=dropout,
                     activation="ReLU",
