4a18ddb1132701dfc455f3b66f864181cf191fe3
melisande-c <milly.croft@gmail.com>
2025-10-17 18:18:19 +0200

    docs: add docs to pad_patch function; refac code slightly

--- src/careamics/dataset_ng/patch_extractor/image_stack/utils.py
+++ src/careamics/dataset_ng/patch_extractor/image_stack/utils.py
@@ -13,16 +13,43 @@ def pad_patch(
     data_shape: Sequence[int],
     patch_data: NDArray[T],
 ) -> NDArray[T]:
+    """
+    Pad patch data with zeros where it is outside the bounds of it's source image.
+
+    This ensures the patch data is contained in an array with the expected patch size.
+
+    If `coords` are negative, the start of the patch will be padded with zeros up until
+    where the start of the image would be, and this is where the patch data starts.
+
+    If the `coords + patch_size` are greater than the bounds of the image then the
+    end of the patch will be filled with zeros.
+
+    Parameters
+    ----------
+    coords : Sequence[int]
+        The coordinates that describe where the patch starts in the spatial dimension of
+        the image
+    patch_size : Sequence[int]
+        The size of the patch in the spatial dimensions.
+    data_shape : Sequence[int]
+        The shape of the image the patch originates from, must be in the format SC(Z)YX.
+    patch_data : NDArray[T]
+        The patch data to be padded.
+
+    Returns
+    -------
+    NDArray[T]
+        The resulting padded patch.
+    """
+    coords_ = np.array(coords)
     patch = np.zeros((data_shape[1], *patch_size), dtype=patch_data.dtype)
-    patch_start = np.clip(np.array(coords), 0, None) - np.array(coords)
-    patch_end = np.array(coords) + np.array(patch_size)
-    patch_end = np.clip(patch_end, None, np.array(data_shape[2:])) - np.clip(
-        np.array(coords), None, np.array(data_shape[2:])
-    )
+    # data start will be zero unless coords are negative
+    data_start = np.clip(coords_, 0, None) - coords_
+    data_end = data_start + np.array(patch_data.shape[1:])
     patch[
         (
-            slice(None, None, None),
-            *tuple(slice(s, t) for s, t in zip(patch_start, patch_end, strict=False)),
+            slice(None, None, None),  # channel slice
+            *tuple(slice(s, t) for s, t in zip(data_start, data_end, strict=False)),
         )
     ] = patch_data
     return patch
