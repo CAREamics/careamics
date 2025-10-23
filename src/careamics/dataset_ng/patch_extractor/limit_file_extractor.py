9b8c04399575cdd7bee8c4ff55c5202ed357a3cf
melisande-c <melisande.croft@fht.org>
2025-10-23 17:05:46 +0200

    feat: working components for iterative file loading - LimitFileExtractor, GroupedIndexSampler, add PatchingStrategy Protocol to return indices relating to each image stack

---
+++ src/careamics/dataset_ng/patch_extractor/limit_file_extractor.py
@@ -0,0 +1,32 @@
+from collections.abc import Sequence
+
+from numpy.typing import NDArray
+
+from .image_stack import FileImageStack
+from .patch_extractor import PatchExtractor
+
+
+class LimitFilePatchExtractor(PatchExtractor):
+
+    def __init__(self, image_stacks: Sequence[FileImageStack]):
+        self.image_stacks: list[FileImageStack]
+        super().__init__(image_stacks)
+        self.loaded_stacks: list[int] = []
+
+    def extract_patch(
+        self,
+        data_idx: int,
+        sample_idx: int,
+        coords: Sequence[int],
+        patch_size: Sequence[int],
+    ) -> NDArray:
+        if data_idx not in self.loaded_stacks:
+            self.image_stacks[data_idx].load()
+            self.loaded_stacks.append(data_idx)
+        # TODO: make maximum images loaded configurable?
+        if len(self.loaded_stacks) > 2:
+            # get the idx that was added longest ago
+            idx_to_close = self.loaded_stacks.pop(0)
+            self.image_stacks[idx_to_close].close()
+
+        return super().extract_patch(data_idx, sample_idx, coords, patch_size)
--- src/careamics/dataset_ng/patching_strategies/whole_sample.py
+++ src/careamics/dataset_ng/patching_strategies/whole_sample.py
@@ -20,6 +20,31 @@ class WholeSamplePatchingStrategy:
     def get_patch_spec(self, index: int) -> PatchSpecs:
         return self.patch_specs[index]
 
+    # Note: this is used by the FileIterSampler
+    def get_patch_indices(self, data_idx: int) -> Sequence[int]:
+        """
+        Get the patch indices will return patches for a specific `image_stack`.
+
+        The `image_stack` corresponds to the given `data_idx`.
+
+        Parameters
+        ----------
+        data_idx : int
+            An index that corresponds to a given `image_stack`.
+
+        Returns
+        -------
+        sequence of int
+            A sequence of patch indices, that when used to index the `CAREamicsDataset
+            will return a patch that comes from the `image_stack` corresponding to the
+            given `data_idx`.
+        """
+        return [
+            i
+            for i, patch_spec in enumerate(self.patch_specs)
+            if patch_spec["data_idx"] == data_idx
+        ]
+
     def _initialize_patch_specs(self) -> list[PatchSpecs]:
         patch_specs: list[PatchSpecs] = []
         for data_idx, data_shape in enumerate(self.data_shapes):
