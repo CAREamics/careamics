b4f015ee3e68a826e15291510d1667266a76d01e
melisande-c <melisande.croft@fht.org>
2025-11-03 10:50:10 +0100

    fix(limit files extractor): close previous image before opening next

--- src/careamics/dataset_ng/patch_extractor/limit_file_extractor.py
+++ src/careamics/dataset_ng/patch_extractor/limit_file_extractor.py
@@ -31,12 +31,13 @@ class LimitFilesPatchExtractor(PatchExtractor):
         patch_size: Sequence[int],
     ) -> NDArray:
         if data_idx not in self.loaded_stacks:
+            # TODO: make maximum images loaded configurable?
+            if len(self.loaded_stacks) >= 1:
+                # get the idx that was added longest ago
+                idx_to_close = self.loaded_stacks.pop(0)
+                self.image_stacks[idx_to_close].close()
+
             self.image_stacks[data_idx].load()
             self.loaded_stacks.append(data_idx)
-        # TODO: make maximum images loaded configurable?
-        if len(self.loaded_stacks) > 1:
-            # get the idx that was added longest ago
-            idx_to_close = self.loaded_stacks.pop(0)
-            self.image_stacks[idx_to_close].close()
 
         return super().extract_patch(data_idx, sample_idx, coords, patch_size)
