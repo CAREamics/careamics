bb81c706b00641bf3a6e22fddfe6a76e66039b76
melisande-c <melisande.croft@fht.org>
2025-11-05 17:47:49 +0100

    docs: docstrings

--- src/careamics/dataset_ng/patch_extractor/patch_construction.py
+++ src/careamics/dataset_ng/patch_extractor/patch_construction.py
@@ -9,6 +9,7 @@ from .image_stack import ImageStack
 
 
 class PatchConstructor(Protocol):
+    """A patch constructor function creates a patch from a given ImageStack."""
 
     def __call__(
         self,
@@ -16,7 +17,26 @@ class PatchConstructor(Protocol):
         sample_idx: int,
         coords: Sequence[int],
         patch_size: Sequence[int],
-    ) -> NDArray[Any]: ...
+    ) -> NDArray[Any]:
+        """
+        Parameters
+        ----------
+        image_stack: ImageStack
+            The image stack to construct a patch from.
+        sample_idx: int
+            Sample index. The first dimension of the image data will be indexed at this
+            value.
+        coords: Sequence of int
+            The coordinates that define the start of a patch.
+        patch_size: Sequence of int
+            The size of the patch in each spatial dimension.
+
+        Returns
+        -------
+        numpy.ndarray
+            The patch.
+        """
+        ...
 
 
 def basic_patch_constr(
@@ -30,9 +50,34 @@ def basic_patch_constr(
     )
 
 
+# closure to create constructor funcs with particular multiscale_count and padding mode
 def lateral_context_patch_constr(
-    multiscale_count: int, padding_mode: Literal["reflect", "wrap"]
+    # TODO: will we stick with this as the parameter name
+    multiscale_count: int,
+    # TODO: add other modes?
+    padding_mode: Literal["reflect", "wrap"],
 ) -> PatchConstructor:
+    """
+    Create a lateral context `PatchConstructor` for MicroSplit.
+
+    Parameters
+    ----------
+    multiscale_count : int
+        The number of multiscale inputs that will be created including the original
+        image size.
+    padding_mode : {"reflect", "wrap"}
+        How lateral context inputs will be padded at the edge of the image. See
+        [`numpy.pad`](https://numpy.org/devdocs/reference/generated/numpy.pad.html) for
+        more information.
+
+    Returns
+    -------
+    PatchConstructor
+        The patch constructor function. It will return patches with the dimensions
+        (L, C, (Z), Y, X) where L will be equal to `multiscale_count`, C is the number
+        of channels in the image, and (Z), Y, X are the patch size.
+    """
+
     def constructor_func(
         image_stack: ImageStack,
         sample_idx: int,
@@ -43,6 +88,11 @@ def lateral_context_patch_constr(
         spatial_shape = shape[2:]
         n_channels = shape[1]
 
+        # There will now be an additional lc dimension,
+        # this has to be handled correctly by the dataset
+        # TODO: maybe we want to limit this constructor to only images with 1 channel
+        #   then we can put LCs in the channel dimension
+        #   but not sure if this artificially limits potential use-cases
         patch = np.zeros((multiscale_count, n_channels, *patch_size))
         for scale in range(multiscale_count):
             lc_patch_size = np.array(patch_size) * (2**scale)
@@ -74,6 +124,7 @@ def lateral_context_patch_constr(
                 pad_width,
                 mode=padding_mode,
             )
+            # TODO: test different downscaling, could try max pooling?
             lc_patch = resize(lc_patch, (n_channels, *patch_size))
             patch[scale] = lc_patch
         return patch
