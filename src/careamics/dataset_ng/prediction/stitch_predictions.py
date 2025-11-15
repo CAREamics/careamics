79307906c26921a2ed5eed1cb92070346258c3c0
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2025-11-15 12:50:41 +0100

    Feat: add NG Dataset stitching

---
+++ src/careamics/dataset_ng/prediction/stitch_predictions.py
@@ -0,0 +1,183 @@
+import builtins
+from collections import defaultdict
+
+import numpy as np
+from numpy.typing import NDArray
+
+from careamics.dataset_ng.dataset import ImageRegionData
+from careamics.dataset_ng.patching_strategies import TileSpecs
+
+
+def sort_tiles_by_data_index(
+    tiles: list[ImageRegionData],
+) -> dict[int, list[ImageRegionData]]:
+    """
+    Sort tiles by their data index.
+
+    Parameters
+    ----------
+    tiles : list of ImageRegionData
+        List of tiles to sort.
+
+    Returns
+    -------
+    {int: list of ImageRegionData}
+        Dictionary mapping data indices to lists of tiles.
+    """
+    sorted_tiles: dict[int, list[ImageRegionData]] = defaultdict(list)
+    for tile in tiles:
+        data_idx = tile.region_spec["data_idx"]
+        sorted_tiles[data_idx].append(tile)
+    return sorted_tiles
+
+
+def sort_tiles_by_sample_index(
+    tiles: list[ImageRegionData],
+) -> dict[int, list[ImageRegionData]]:
+    """
+    Sort tiles by their sample index.
+
+    Parameters
+    ----------
+    tiles : list of ImageRegionData
+        List of tiles to sort.
+
+    Returns
+    -------
+    {int: list of ImageRegionData}
+        Dictionary mapping sample indices to lists of tiles.
+    """
+    sorted_tiles: dict[int, list[ImageRegionData]] = defaultdict(list)
+    for tile in tiles:
+        sample_idx = tile.region_spec["sample_idx"]
+        sorted_tiles[sample_idx].append(tile)
+    return sorted_tiles
+
+
+def stitch_prediction(
+    tiles: list[ImageRegionData],
+) -> list[NDArray]:
+    """
+    Stitch tiles back together to form full images.
+
+    Tiles are of dimensions SC(Z)YX, where C is the number of channels and can be a
+    singleton dimension.
+
+    Parameters
+    ----------
+    tiles : list of ImageRegionData
+        Cropped tiles and their respective stitching coordinates. Can contain tiles
+        from multiple images.
+
+    Returns
+    -------
+    list of numpy.ndarray
+        Full images, may be a single image.
+    """
+    # sort tiles by data index
+    sorted_tiles: dict[int, list[ImageRegionData]] = sort_tiles_by_data_index(tiles)
+
+    # stitch each image separately
+    image_predictions: list[NDArray] = []
+    for data_idx in sorted_tiles.keys():
+        image_predictions.append(stitch_single_prediction(sorted_tiles[data_idx]))
+    return image_predictions
+
+
+def stitch_single_prediction(
+    tiles: list[ImageRegionData],
+) -> NDArray:
+    """
+    Stitch tiles back together to form a full image.
+
+    Tiles are of dimensions SC(Z)YX, where C is the number of channels and can be a
+    singleton dimension.
+
+    Parameters
+    ----------
+    tiles : list of ImageRegionData
+        Cropped tiles and their respective stitching coordinates.
+
+    Returns
+    -------
+    numpy.ndarray
+        Full image, with dimensions SC(Z)YX.
+    """
+    data_shape = tiles[0].data_shape
+    predicted_image = np.zeros(data_shape, dtype=np.float32)
+
+    if "S" in tiles[0].axes:
+        tiles_by_sample = sort_tiles_by_sample_index(tiles)
+        for sample_idx in tiles_by_sample.keys():
+            sample_tiles = tiles_by_sample[sample_idx]
+            stitched_sample = stitch_single_sample(sample_tiles)
+
+            # compute sample slice
+            sample_slice = slice(
+                sample_idx,
+                sample_idx + 1,
+            )
+
+            # insert stitched sample into predicted image
+            predicted_image[sample_slice] = stitched_sample.astype(np.float32)
+    else:
+        # stitch as a single sample
+        predicted_image = stitch_single_sample(tiles)
+
+    return predicted_image
+
+
+def stitch_single_sample(
+    tiles: list[ImageRegionData],
+) -> NDArray:
+    """
+    Stitch tiles back together to form a full sample.
+
+    Tiles are of dimensions C(Z)YX, where C is the number of channels and can be a
+    singleton dimension.
+
+    Parameters
+    ----------
+    tiles : list of ImageRegionData
+        Cropped tiles and their respective stitching coordinates.
+
+    Returns
+    -------
+    numpy.ndarray
+        Full sample, with dimensions C(Z)YX.
+    """
+    data_shape = tiles[0].data_shape
+    len_patches = len(tiles[0].data.squeeze().shape)
+
+    predicted_sample = np.zeros(data_shape[-len_patches:], dtype=np.float32)
+
+    for tile in tiles:
+        # compute crop coordinates and stitiching coordinates
+        tile_spec: TileSpecs = tile.region_spec  # type: ignore
+        crop_coords = tile_spec["crop_coords"]
+        crop_size = tile_spec["crop_size"]
+        stitch_coords = tile_spec["stitch_coords"]
+
+        crop_slices: tuple[builtins.ellipsis | slice, ...] = (
+            ...,
+            *[
+                slice(start, start + length)
+                for start, length in zip(crop_coords, crop_size, strict=True)
+            ],
+        )
+
+        stitch_slices: tuple[builtins.ellipsis | slice, ...] = (
+            ...,
+            *[
+                slice(start, start + length)
+                for start, length in zip(stitch_coords, crop_size, strict=True)
+            ],
+        )
+
+        # crop predited tile according to overlap coordinates
+        cropped_tile = tile.data[crop_slices]
+
+        # insert cropped tile into predicted image
+        predicted_sample[stitch_slices] = cropped_tile.astype(np.float32)
+
+    return predicted_sample
---
+++ tests/dataset_ng/prediction/test_stitch_prediction.py
@@ -0,0 +1,177 @@
+import numpy as np
+import pytest
+from numpy.typing import NDArray
+
+from careamics.dataset_ng.dataset import ImageRegionData
+from careamics.dataset_ng.patch_extractor.patch_extractor_factory import (
+    create_array_extractor,
+)
+from careamics.dataset_ng.patching_strategies import TileSpecs, TilingStrategy
+from careamics.dataset_ng.prediction.stitch_predictions import (
+    sort_tiles_by_data_index,
+    sort_tiles_by_sample_index,
+    stitch_prediction,
+    stitch_single_prediction,
+    stitch_single_sample,
+)
+
+# TODO will be redundant once tiling_strategy tests will not use the old stitching
+
+
+@pytest.fixture
+def tiles(n_data, shape, axes) -> tuple[NDArray, list[ImageRegionData]]:
+    # create data
+    array = np.arange(n_data * np.prod(shape)).reshape((n_data, *shape))
+
+    # create tiling strategy
+    if "Z" in axes:
+        tile_size = (8, 16, 16)
+        overlaps = (2, 4, 4)
+    else:
+        tile_size = (16, 16)
+        overlaps = (4, 4)
+
+    if "S" in axes:
+        if "C" in axes:
+            shape_with_sc = shape
+        else:
+            shape_with_sc = (shape[0], 1, *shape[1:])
+    else:
+        if "C" in axes:
+            shape_with_sc = (1, *shape)
+        else:
+            shape_with_sc = (1, 1, *shape)
+
+    tiling_strategy = TilingStrategy(
+        data_shapes=[shape_with_sc] * n_data, tile_size=tile_size, overlaps=overlaps
+    )
+    n_tiles = tiling_strategy.n_patches
+
+    # create patch extractor
+    patch_extractor = create_array_extractor(
+        source=[array[i] for i in range(n_data)], axes=axes
+    )
+
+    # extract tiles
+    tiles: list[ImageRegionData] = []
+    for i in range(n_tiles):
+        tile_spec: TileSpecs = tiling_strategy.get_patch_spec(i)
+        tile = patch_extractor.extract_patch(
+            data_idx=tile_spec["data_idx"],
+            sample_idx=tile_spec["sample_idx"],
+            coords=tile_spec["coords"],
+            patch_size=tile_spec["patch_size"],
+        )
+        tiles.append(
+            ImageRegionData(
+                data=tile,
+                source="array",
+                dtype=str(tile.dtype),
+                data_shape=shape,
+                axes=axes,
+                region_spec=tile_spec,
+            )
+        )
+    return array, tiles
+
+
+# parameters are injected automatically in the tiles fixture
+@pytest.mark.parametrize("n_data", [1, 3])
+@pytest.mark.parametrize(
+    "shape, axes",
+    [
+        ((32, 32), "YX"),
+    ],
+)
+def test_sort_tiles_by_data_index(tiles):
+    array, tile_list = tiles
+    n_data = array.shape[0]
+
+    sorted_tiles = sort_tiles_by_data_index(tile_list)
+    assert len(sorted_tiles.keys()) == n_data
+    n_tiles_per_data = [len(sorted_tiles[data_idx]) for data_idx in sorted_tiles.keys()]
+    assert all(n_tiles_per_data[0] == n_tiles_per_data[i] for i in range(1, n_data))
+
+
+# parameters are injected automatically in the tiles fixture
+@pytest.mark.parametrize(
+    "n_data, shape, axes",
+    [
+        (1, (5, 32, 32), "SYX"),
+    ],
+)
+def test_sort_tiles_by_sample_index(tiles):
+    array, tile_list = tiles
+    n_data = array.shape[1]
+
+    sorted_tiles = sort_tiles_by_sample_index(tile_list)
+    assert len(sorted_tiles.keys()) == n_data
+    n_tiles_per_data = [len(sorted_tiles[data_idx]) for data_idx in sorted_tiles.keys()]
+    assert all(n_tiles_per_data[0] == n_tiles_per_data[i] for i in range(1, n_data))
+
+
+# parameters are injected automatically in the tiles fixture
+@pytest.mark.parametrize("n_data", [1])
+@pytest.mark.parametrize(
+    "shape, axes",
+    [
+        ((32, 32), "YX"),
+        ((3, 32, 32), "CYX"),
+        ((8, 32, 32), "ZYX"),
+        ((3, 8, 32, 32), "CZYX"),
+    ],
+)
+def test_stitching_single_sample(tiles):
+    array, tile_list = tiles
+    array = array.squeeze()  # remove sample dim
+
+    stitched_array = stitch_single_sample(tile_list)
+    np.testing.assert_array_equal(stitched_array, array)
+
+
+# parameters are injected automatically in the tiles fixture
+@pytest.mark.parametrize("n_data", [1])
+@pytest.mark.parametrize(
+    "shape, axes",
+    [
+        ((32, 32), "YX"),
+        ((3, 32, 32), "CYX"),
+        ((5, 32, 32), "SYX"),
+        ((8, 32, 32), "ZYX"),
+        ((3, 8, 32, 32), "CZYX"),
+        ((5, 3, 32, 32), "SCYX"),
+        ((5, 8, 32, 32), "SZYX"),
+        ((5, 3, 8, 32, 32), "SCZYX"),
+    ],
+)
+def test_stitching_single_prediction(tiles):
+    array, tile_list = tiles
+    array = array.squeeze()  # remove sample dim if S=1
+
+    stitched_array = stitch_single_prediction(tile_list)
+    np.testing.assert_array_equal(stitched_array, array)
+
+
+# parameters are injected automatically in the tiles fixture
+@pytest.mark.parametrize("n_data", [4])
+@pytest.mark.parametrize(
+    "shape, axes",
+    [
+        ((32, 32), "YX"),
+        ((3, 32, 32), "CYX"),
+        ((5, 32, 32), "SYX"),
+        ((8, 32, 32), "ZYX"),
+        ((3, 8, 32, 32), "CZYX"),
+        ((5, 3, 32, 32), "SCYX"),
+        ((5, 8, 32, 32), "SZYX"),
+        ((5, 3, 8, 32, 32), "SCZYX"),
+    ],
+)
+def test_stitching_predicition(tiles):
+    array, tile_list = tiles
+    array = array.squeeze()  # remove sample dim if S=1
+
+    stitched_arrays = stitch_prediction(tile_list)
+    prediction = np.stack(stitched_arrays)
+
+    np.testing.assert_array_equal(prediction, array)
