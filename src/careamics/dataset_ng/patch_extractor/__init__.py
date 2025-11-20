aa0c142b50882dd34a3ef6bb26fbfbc0628cd375
melisande-c <melisande.croft@fht.org>
2025-11-20 15:34:30 +0100

    fix: imports

--- src/careamics/dataset_ng/patch_extractor/__init__.py
+++ src/careamics/dataset_ng/patch_extractor/__init__.py
@@ -1,11 +1,7 @@
 __all__ = [
-    "GenericImageStack",
-    "ImageStackLoader",
     "LimitFilesPatchExtractor",
     "PatchExtractor",
 ]
 
-from ..image_stack import GenericImageStack
-from ..image_stack_loader.image_stack_loader_protocol import ImageStackLoader
 from .limit_file_extractor import LimitFilesPatchExtractor
 from .patch_extractor import PatchExtractor
