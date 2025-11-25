583e1ca1d8b7014065282ac25dde08403754234b
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2025-11-25 11:36:23 +0100

    Chore(config): Add imports to `__init__` (#633)
    ## Description
    
    <!-- This section provides the necessary background and information for
    reviewers to
    understand the code and have the correct mindset when examining changes.
    -->
    
    The config models can be deeply buried, this PR brings about more
    reasonable import levels in the config modules.
    
    I have not updated the imports throughout the code, this can be done
    progressively. This was motivated by fixing imports in the example CI.

--- src/careamics/config/data/__init__.py
+++ src/careamics/config/data/__init__.py
@@ -2,8 +2,26 @@
 
 __all__ = [
     "DataConfig",
+    "MaskFilterConfig",
+    "MaxFilterConfig",
+    "MeanSTDFilterConfig",
     "NGDataConfig",
+    "RandomPatchingConfig",
+    "ShannonFilterConfig",
+    "TiledPatchingConfig",
+    "WholePatchingConfig",
 ]
 
 from .data_config import DataConfig
 from .ng_data_config import NGDataConfig
+from .patch_filter import (
+    MaskFilterConfig,
+    MaxFilterConfig,
+    MeanSTDFilterConfig,
+    ShannonFilterConfig,
+)
+from .patching_strategies import (
+    RandomPatchingConfig,
+    TiledPatchingConfig,
+    WholePatchingConfig,
+)
