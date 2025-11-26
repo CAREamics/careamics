e338bccaacde2ef4fc985db2086e3edcf743ea87
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2025-11-26 16:54:26 +0100

    Feat(Next-Gen Dataset): enforce axes in CZI image stack loader (#635)
    ## Description
    
    <!-- This section provides the necessary background and information for
    reviewers to
    understand the code and have the correct mindset when examining changes.
    -->
    
    Another problem I ran into while testing the internal CZI CI: one can
    pass set of axes that are not compatible (e.g. missing `S` and `C`) to
    the `NGDataConfig` because neither `czi` image stack loader
    (`load_czis`) nor `CziImageStack` check the axes. All other `ImageStack`
    call `reshape_axes`, which raises errors when there are inconsistencies
    between shape and axes.
    
    This PR adds a check on the `axes` in `load_czis`, and corresponding
    tests.

--- src/careamics/config/validators/__init__.py
+++ src/careamics/config/validators/__init__.py
@@ -2,15 +2,17 @@
 
 __all__ = [
     "check_axes_validity",
+    "check_czi_axes_validity",
     "model_matching_in_out_channels",
     "model_without_final_activation",
     "model_without_n2v2",
     "patch_size_ge_than_8_power_of_2",
 ]
 
+from .axes_validators import check_axes_validity, check_czi_axes_validity
 from .model_validators import (
     model_matching_in_out_channels,
     model_without_final_activation,
     model_without_n2v2,
 )
-from .validator_utils import check_axes_validity, patch_size_ge_than_8_power_of_2
+from .patch_validators import patch_size_ge_than_8_power_of_2
---
+++ src/careamics/dataset_ng/image_stack/image_utils/__init__.py
@@ -0,0 +1,12 @@
+"""Image stack utilities."""
+
+__all__ = [
+    "pad_patch",
+    "reshaped_array_shape",
+]
+
+
+from .image_stack_utils import (
+    pad_patch,
+    reshaped_array_shape,
+)
