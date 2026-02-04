0073614290625a1f88af3238166c4eef85e0650a
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2026-02-04 16:35:18 +0100

    Feat: prediction writer not enabled at init

--- src/careamics/careamist_v2.py
+++ src/careamics/careamist_v2.py
@@ -132,7 +132,6 @@ class CAREamistV2:
 
         # init callbacks
         self.prediction_writer = PredictionWriterCallback(self.work_dir)
-        self.prediction_writer.enable_writing(True)
         self.callbacks = self._define_callbacks(
             user_context["callbacks"], self.config, self.work_dir
         )
