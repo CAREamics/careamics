fcf9da5e973522eb640bfc571b44ad69b892eb2b
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2026-02-26 13:56:06 +0100

    Update src/careamics/lightning/dataset_ng/metrics/psnr.py
    Co-authored-by: Vera Galinova <32124316+veegalinova@users.noreply.github.com>

--- src/careamics/lightning/dataset_ng/metrics/psnr.py
+++ src/careamics/lightning/dataset_ng/metrics/psnr.py
@@ -322,8 +322,8 @@ class RangelessPSNR(Metric):
         mse = torch.mean((target - preds) ** 2 + self.eps, dim=dims)
 
         # update states
-        self.psnr_sum += torch.sum(10 * torch.log10(data_range**2 / mse), dim=0)
-        self.total += batch_size
+        self.psnr_sum: torch.Tensor = self.psnr_sum + torch.sum(10 * torch.log10(data_range**2 / mse), dim=0)
+        self.total: torch.Tensor = self.total + batch_size
 
     def compute(self) -> Tensor | dict[str, Tensor]:
         """Compute the final metric value.
