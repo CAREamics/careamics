329ce3d2b94ffe5c86f18200cdf2445a3d851d3b
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2026-02-27 16:15:06 +0100

    Feat(Next-Gen): add CARE and N2N configurations and factories (#778)
    ## Description
    
    <!-- This section provides the necessary background and information for
    reviewers to
    understand the code and have the correct mindset when examining changes.
    -->
    
    > [!NOTE]  
    > **tldr**: Adding convenience functions to create CARE and N2N
    configurations.
    
    
    ### Background - why do we need this PR?
    
    <!-- What problem are you solving? Describe in a few sentences the state
    before
    this PR. Use code examples if useful. -->
    
    The convenience functions are the user entry point into configuring
    CAREamics.
    
    
    ## Changes Made
    
    <!-- This section highlights the important features and files that
    reviewers should
    pay attention to when reviewing. Only list important features or files,
    this is useful for
    reviewers to correctly assess how deeply the modifications impact the
    code base.
    
    For instance:
    
    ### New features or files
    - `NewClass` added to `new_file.py`
    - `new_function` added to `existing_file.py`
    
    ...
    -->
    
    Added the following convenience functions:
    - `create_care_config`
    - `create_advanced_care_config`
    - `create_n2n_config`
    - `create_advanced_n2n_config`
    
    Note that these all share an underlying
    `_create_advanced_supervised_config` since they essentially require the
    same parameters.
    
    
    ## Related Issues
    
    <!-- Link to any related issues or discussions. Use keywords like
    "Fixes", "Resolves",
    or "Closes" to link to issues automatically. -->
    
    - Closes https://github.com/CAREamics/careamics/issues/732
    
    ## Additional Notes and Examples
    
    <!-- Provide any additional information that will help reviewers
    understand the
    changes. This can be links to documentations, forum posts, past
    discussions etc. -->
    
    Note that the only reason that N2N and CARE are separated is to allow
    clarity for users (one require noisy data) and proper citation when
    exporting to BMZ. We could discuss whether this is something we want to
    continue maintaining.
    
    ---
    
    **Please ensure your PR meets the following requirements:**
    
    - [x] Code builds and passes tests locally, including doctests
    - [x] New tests have been added (for bug fixes/features)
    - [x] Pre-commit passes
    - [ ] PR to the documentation exists (for bug fixes / features)

--- src/careamics/config/ng_factories/__init__.py
+++ src/careamics/config/ng_factories/__init__.py
@@ -1,12 +1,22 @@
 """Convenience functions to create coherent configurations for CAREamics."""
 
 __all__ = [
+    "create_advanced_care_config",
+    "create_advanced_n2n_config",
     "create_advanced_n2v_config",
+    "create_care_config",
+    "create_n2n_config",
     "create_n2v_config",
     "create_ng_data_configuration",
     "create_structn2v_config",
 ]
 
+from .care_n2n_factory import (
+    create_advanced_care_config,
+    create_advanced_n2n_config,
+    create_care_config,
+    create_n2n_config,
+)
 from .data_factory import create_ng_data_configuration
 from .n2v_factory import (
     create_advanced_n2v_config,
