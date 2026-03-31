e6adaae1ac0061b8de5327353e2cead79977d5b9
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2026-03-31 15:38:34 +0200

    Fix: fix imports

---
+++ src/careamics/compat/config/lightning/__init_.py
@@ -0,0 +1,10 @@
+"""Deprecated module."""
+
+from .callbacks import CheckpointConfig, EarlyStoppingConfig
+from .training_config import TrainingConfig
+
+__all__ = [
+    "CheckpointConfig",
+    "EarlyStoppingConfig",
+    "TrainingConfig",
+]
