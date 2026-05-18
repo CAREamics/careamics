618c637d9368ca5be2472656e6f433ec86d2d20d
melisande-c <melisande.croft@fht.org>
2026-05-18 20:50:10 +0200

    feat: add patch constructor protocol

---
+++ src/careamics/dataset/patch_extractor/patch_constructor/__init__.py
@@ -0,0 +1,5 @@
+"""Patch constructor package. Patch constructors output a patch for a given index."""
+
+__all__ = ["PatchConstructor"]
+
+from .patch_constructor import PatchConstructor
---
+++ src/careamics/dataset/patch_extractor/patch_constructor/patch_constructor.py
@@ -0,0 +1,56 @@
+"""PatchConstructor Protocol."""
+
+from typing import Any, Protocol
+
+from numpy.typing import NDArray
+
+from careamics.dataset.patching import PatchSpecs
+
+
+class PatchConstructor(Protocol):
+    """Module for extracting and constructing inputs and targets."""
+
+    @property
+    def n_patches(self):
+        """The number of patches."""
+        ...
+
+    def construct_patch(
+        self, index: int
+    ) -> tuple[NDArray[Any], NDArray[Any] | None, PatchSpecs]:
+        """Construct the patch that will be input into the model.
+
+        Parameters
+        ----------
+        index : int
+            The index of the patch, the index has to be less than `n_patches`.
+
+        Returns
+        -------
+        input : NDArray[Any]
+            The input patch.
+        target : NDArray[Any] | None
+            The target patch.
+        patch_spec : PatchSpecs
+            The patch specification.
+        """
+        ...
+
+    # e.g. for MicroSplit the full input has the lateral context.
+    #      we need to remove the lateral context to calculate the normalization stats.
+    def get_principal_input(self, input: NDArray[Any]) -> NDArray[Any]:
+        """Get the principle input.
+
+        This is useful for tasks such as the calculation of stats for normalization.
+
+        Parameters
+        ----------
+        input : NDArray[Any]
+            The complete input.
+
+        Returns
+        -------
+        NDArray[Any]
+            The principal input.
+        """
+        ...
