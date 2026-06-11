1b03ce968a1132b6ef5cb84758e402e77aeadb89
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2026-06-11 13:54:09 +0200

    Fix: fix test running on wrong condition

--- tests/unit/lightning/modules/n2v_utils/test_struct_mask_parameters.py
+++ tests/unit/lightning/modules/n2v_utils/test_struct_mask_parameters.py
@@ -5,7 +5,7 @@ from careamics.lightning.modules.n2v_utils.struct_mask_parameters import (
     StructMaskParameters,
 )
 
-STRUCT_AXIS = [v.value for v in SupportedStructAxis]
+STRUCT_AXIS = [v.value for v in SupportedStructAxis if v != SupportedStructAxis.NONE]
 
 
 @pytest.mark.parametrize("span", [3, 5])
