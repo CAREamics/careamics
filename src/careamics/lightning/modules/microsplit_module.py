5f37d13235d864b5ac7c1a541532916757040505
zubarev.ia@gmail.com <zubarev.ia@gmail.com>
2026-07-16 15:31:04 +0200

    additional_metadata fix

--- src/careamics/lightning/modules/microsplit_module.py
+++ src/careamics/lightning/modules/microsplit_module.py
@@ -321,7 +321,7 @@ class MicroSplitModule(L.LightningModule):
             dtype=x.dtype,
             axes=x.axes,
             region_spec=x.region_spec,
-            additional_metadata={},
+            additional_metadata=x.additional_metadata,
             original_data_shape=x.original_data_shape,
         )
 
