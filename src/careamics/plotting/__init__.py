bf9f9b263068513eddf6d41c8f7cf7e6cf209a0f
Mehdi Seifi <mehdiseifi@gmail.com>
2026-08-12 16:34:24 +0200

    added noise residuals plot

--- src/careamics/plotting/__init__.py
+++ src/careamics/plotting/__init__.py
@@ -2,8 +2,10 @@
 
 from .loss_plot import plot_loss
 from .noise_model import plot_noise_model_distribution
+from .noise_residual import plot_noise_residuals
 
 __all__ = [
     "plot_loss",
     "plot_noise_model_distribution",
+    "plot_noise_residuals",
 ]
