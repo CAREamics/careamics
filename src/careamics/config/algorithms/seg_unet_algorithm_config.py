c421457b3a1ee3257257787c25123e6e9e21d626
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2026-08-20 14:19:29 +0200

    Refac: change function name

--- src/careamics/config/algorithms/seg_unet_algorithm_config.py
+++ src/careamics/config/algorithms/seg_unet_algorithm_config.py
@@ -13,7 +13,7 @@ from careamics.config.validators import (
 )
 
 
-def _model_at_least_2_classes(model: UNetConfig) -> UNetConfig:
+def _model_with_at_least_2_classes(model: UNetConfig) -> UNetConfig:
     """Validate that the Unet model has at least two classes.
 
     Parameters
@@ -53,7 +53,7 @@ class SegAlgorithm(UNetBasedAlgorithm):
         UNetConfig,
         AfterValidator(model_without_n2v2),
         AfterValidator(model_without_final_activation),
-        AfterValidator(_model_at_least_2_classes),
+        AfterValidator(_model_with_at_least_2_classes),
     ]
     """UNet without a final activation function and without the `n2v2` modifications."""
 
