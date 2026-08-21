4833712a9fd313cd5a8710db9eb2602750d2d576
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2026-08-21 14:55:48 +0200

    Refac: remove method overwriting

--- src/careamics/models/constraints/seg_unet_constraints.py
+++ src/careamics/models/constraints/seg_unet_constraints.py
@@ -26,29 +26,6 @@ class SegUNetConstraints:
         """
         self.model_config = model_config
 
-    def validate_input_channels(self, n_channels: int) -> None:
-        """Whether the given channel size is compatible with the model constraints.
-
-        Parameters
-        ----------
-        n_channels : int
-            The number of channels in the input tensor to validate.
-
-        Raises
-        ------
-        ValueError
-            If the number of channels is not compatible with the model constraints.
-        """
-        # validate input channels
-        if n_channels != self.model_config.get_num_input_channels():
-            raise ValueError(
-                f"Number of channels in input image ({n_channels}) does not match the "
-                f"number of input channels expected by the model configuration "
-                f"({self.model_config.get_num_input_channels()}). Use the `channels` "
-                f"parameter to specify a subset of channels, or adjust the number of "
-                f"input channels in the configuration to match your data."
-            )
-
     def validate_target_channels(self, n_channels: int) -> None:
         """Whether the given channel size is compatible with the model constraints.
 
