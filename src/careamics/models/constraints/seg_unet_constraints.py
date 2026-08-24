2b72cd4f8461b8a8b59e23dfa957331e3473a74a
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2026-08-24 16:29:34 +0200

    Feat: update seg constraints

--- src/careamics/models/constraints/seg_unet_constraints.py
+++ src/careamics/models/constraints/seg_unet_constraints.py
@@ -1,14 +1,14 @@
 """Segmentation UNet model constraints."""
 
-from collections.abc import Sequence
-
 from careamics.config.architectures import UNetConfig
+
+from .unet_constraints import UNetConstraints
 
 
 # TODO the SegUnet is now testing the number of channels of the target, meaning
 # that it acts more as a task constraint validator than a model one.
-class SegUNetConstraints:
-    """SegmentationUNet model constraints on input and output tensors.
+class SegUNetConstraints(UNetConstraints):
+    """Segmentation UNet model constraints on input and output tensors.
 
     Parameters
     ----------
@@ -48,39 +48,3 @@ class SegUNetConstraints:
                 f"targets are one-hot encoded, adjust your data to have a single "
                 f"channel with integer class labels."
             )
-
-    def validate_spatial_shape(self, input_shape: Sequence[int]) -> None:
-        """Whether the given spatial shape is compatible with the model constraints.
-
-        Shape must be of length 2 (YX) or 3 (ZYX). To validate channel dimension, use
-        `validate_input_channels` or `validate_target_channels` instead.
-
-        Parameters
-        ----------
-        input_shape : Sequence[int]
-            The spatial shape of the input tensor to validate (length 2 or 3).
-
-        Raises
-        ------
-        ValueError
-            If the spatial shape is not compatible with the model constraints.
-        """
-        if len(input_shape) not in (2, 3):
-            raise ValueError(
-                f"Spatial input shape to model constraints should have length 2 (YX) or"
-                f" 3 (ZYX), but got shape {input_shape}."
-            )
-
-        dim_label = "ZYX" if len(input_shape) == 3 else "YX"
-
-        # check spatial dims against model depth constraints
-        depth = self.model_config.depth
-        for i, dim in enumerate(input_shape):
-            if dim % (2**depth) != 0 or dim == 0:
-                raise ValueError(
-                    f"Input data dimension {dim_label[i]} (size {dim}) is not a "
-                    f"multiple of {2**depth} (2 to the power of the model depth). If "
-                    f"you are training, adjust `patch_size`. If you are predicting,"
-                    f" your input data shape is not compatible, use tiling by passing "
-                    f"`tile_size`. If you are already using tiling, adjust `tile_size`."
-                )
