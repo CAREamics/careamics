07cd1b0ff07e2512d964959ee8664abc83c3bd1e
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2026-09-09 16:44:24 +0200

    Fix: path

--- examples/ome-ngff/n2v_lightsheet_denoising.py
+++ examples/ome-ngff/n2v_lightsheet_denoising.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
-"""Configure N2V denoising for a 3D OME-NGFF light-sheet image."""
+"""Configure N2V denoising for a 3D OME-NGFF light-sheet image.
+
+Data from Hess, M., Caton, M., Kothari, M., Swedlow, J., Liberali, P., & Quintas Glasner
+de Medeiros, G. (2026). Reference OME-Zarr for 3D time-lapse light-sheet microscopy with
+nuclei tracking (Version v0.1) [Dataset]. Zenodo.
+https://doi.org/10.5281/zenodo.22078388
+"""
 
 from pathlib import Path
 
@@ -18,20 +24,16 @@ def download_data() -> Path:
     Path
         Path to data.
     """
-    file_list = Path(
-        pooch.retrieve(
-            "https://zenodo.org/records/22078388/files/001-small-lowT.zip?download=1",
-            known_hash=(
-                "5069b9606d646f810e9f0b899d98916d8366bfb14bc74765774c1d4df4a5fe80"
-            ),
-            processor=pooch.Unzip(),
-            path=Path("data"),
-        )
+    file_list = pooch.retrieve(
+        "https://zenodo.org/records/22078388/files/001-small-lowT.zip?download=1",
+        known_hash=("5069b9606d646f810e9f0b899d98916d8366bfb14bc74765774c1d4df4a5fe80"),
+        processor=pooch.Unzip(),
+        path=Path(__file__).parent / "data",
     )
 
     for f in file_list:
         if f.endswith("raw.ome.zarr/zarr.json"):
-            return f
+            return Path(f).parent
 
     return f
 
@@ -52,11 +54,13 @@ def main() -> None:
     config = create_advanced_n2v_config(
         experiment_name="n2v_lightsheet_denoising",
         data_type="zarr",
-        axes="ZYX",
+        axes="TCZYX",
         patch_size=(16, 128, 128),
-        batch_size=8,
+        batch_size=2,
         num_epochs=10,
         num_steps=100,
+        n_channels=2,
+        independent_channels=True,
     )
     careamist = CAREamist(config=config, work_dir=root)
     careamist.train(train_data=path_to_zarr)
